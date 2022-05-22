3da84b87d580cfcad3992f7be45ba31ee981f457
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-05-22 14:23:52 +0530

    Update index.php

--- Modules/api/markscard/index.php
+++ Modules/api/markscard/index.php
@@ -51,39 +51,89 @@ error_reporting(E_ALL & ~E_NOTICE);
 
 ?>
 <?php
-function numberTowords(float $number)
+function numberTowords($num)
 {
-    $decimal = round($number - ($no = floor($number)), 2) * 100;
-    $hundred = null;
-    $digits_length = strlen($no);
-    $i = 0;
-    $str = array();
-    $words = array(0 => 'Zero', 1 => 'One', 2 => 'Two',
-        3 => 'Three', 4 => 'Four', 5 => 'Five', 6 => 'Six',
-        7 => 'Seven', 8 => 'Eight', 9 => 'Nine',
-        10 => 'Ten', 11 => 'Eleven', 12 => 'Twelve',
-        13 => 'Thirteen', 14 => 'Fourteen', 15 => 'Fifteen',
-        16 => 'Sixteen', 17 => 'Seventeen', 18 => 'Eighteen',
-        19 => 'Nineteen', 20 => 'Twenty', 30 => 'Thirty',
-        40 => 'Forty', 50 => 'Fifty', 60 => 'Sixty',
-        70 => 'Seventy', 80 => 'Eighty', 90 => 'Ninety');
-    $digits = array('', 'Hundred','Thousand','Lakh', 'Crore');
-    while( $i < $digits_length ) {
-        $divider = ($i == 2) ? 10 : 100;
-        $number = floor($no % $divider);
-        $no = floor($no / $divider);
-        $i += $divider == 10 ? 1 : 2;
-        if ($number) {
-            $plural = (($counter = count($str)) && $number > 9) ? 's' : null;
-            $hundred = ($counter == 1 && $str[0]) ? 'and' : null;
-            $str [] = ($number < 21) ? $words[$number].' '. $digits[$counter]. $plural.' '.$hundred:$words[floor($number / 10) * 10].' '.$words[$number % 10]. ' '.$digits[$counter].$plural.' '.$hundred;
-        } else $str[] = null;
-    }
-    $Rupees = implode('', array_reverse($str));
-    $paise = ($decimal > 0) ? "." . ($words[$decimal / 10] . " " . $words[$decimal % 10]) . ' ' : '';
-    return ($Rupees ? $Rupees . ' ' : '') . $paise;
-}
 
+$ones = array(
+0 =>"ZERO",
+1 => "ONE",
+2 => "TWO",
+3 => "THREE",
+4 => "FOUR",
+5 => "FIVE",
+6 => "SIX",
+7 => "SEVEN",
+8 => "EIGHT",
+9 => "NINE",
+10 => "TEN",
+11 => "ELEVEN",
+12 => "TWELVE",
+13 => "THIRTEEN",
+14 => "FOURTEEN",
+15 => "FIFTEEN",
+16 => "SIXTEEN",
+17 => "SEVENTEEN",
+18 => "EIGHTEEN",
+19 => "NINETEEN",
+"014" => "FOURTEEN"
+);
+$tens = array( 
+0 => "ZERO",
+1 => "TEN",
+2 => "TWENTY",
+3 => "THIRTY", 
+4 => "FORTY", 
+5 => "FIFTY", 
+6 => "SIXTY", 
+7 => "SEVENTY", 
+8 => "EIGHTY", 
+9 => "NINETY" 
+); 
+$hundreds = array( 
+"HUNDRED", 
+"THOUSAND", 
+"MILLION", 
+"BILLION", 
+"TRILLION", 
+"QUARDRILLION" 
+); /*limit t quadrillion */
+$num = number_format($num,2,".",","); 
+$num_arr = explode(".",$num); 
+$wholenum = $num_arr[0]; 
+$decnum = $num_arr[1]; 
+$whole_arr = array_reverse(explode(",",$wholenum)); 
+krsort($whole_arr,1); 
+$rettxt = ""; 
+foreach($whole_arr as $key => $i){
+	
+while(substr($i,0,1)=="0")
+		$i=substr($i,1,5);
+if($i < 20){ 
+/* echo "getting:".$i; */
+$rettxt .= $ones[$i]; 
+}elseif($i < 100){ 
+if(substr($i,0,1)!="0")  $rettxt .= $tens[substr($i,0,1)]; 
+if(substr($i,1,1)!="0") $rettxt .= " ".$ones[substr($i,1,1)]; 
+}else{ 
+if(substr($i,0,1)!="0") $rettxt .= $ones[substr($i,0,1)]." ".$hundreds[0]; 
+if(substr($i,1,1)!="0")$rettxt .= " ".$tens[substr($i,1,1)]; 
+if(substr($i,2,1)!="0")$rettxt .= " ".$ones[substr($i,2,1)]; 
+} 
+if($key > 0){ 
+$rettxt .= " ".$hundreds[$key]." "; 
+}
+} 
+if($decnum > 0){
+$rettxt .= " and ";
+if($decnum < 20){
+$rettxt .= $ones[$decnum];
+}elseif($decnum < 100){
+$rettxt .= $tens[substr($decnum,0,1)];
+$rettxt .= " ".$ones[substr($decnum,1,1)];
+}
+}
+return $rettxt;
+}
 extract($_POST);
 if(isset($convert))
 {
