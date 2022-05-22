00b6133d9e64648969a71187c7fccb5937debb93
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-05-22 14:20:27 +0530

    Update index.php

--- Modules/api/markscard/index.php
+++ Modules/api/markscard/index.php
@@ -75,8 +75,8 @@ function numberTowords(float $number)
         $i += $divider == 10 ? 1 : 2;
         if ($number) {
             $plural = (($counter = count($str)) && $number > 9) ? 's' : null;
-            $hundred = ($counter == 1 && $str[0]) ? 'and' : null;
-            $str [] = ($number < 21) ? $words[$number].' '. $digits[$counter]. $plural.' '.$hundred:$words[floor($number / 10) * 10].' '.$words[$number % 10]. ' '.$digits[$counter].$plural.' '.$hundred;
+            $hundred = ($counter == 1 && $str[0]) ? 'and ' : null;
+            $str [] = ($number < 21) ? $words[$number].' '. $digits[$counter]. $plural.' '.$hundred:$words[floor($number / 10) * 10].''.$words[$number % 10]. ' '.$digits[$counter].$plural.' '.$hundred;
         } else $str[] = null;
     }
     $Rupees = implode('', array_reverse($str));
