40146a981b60f707ea59a485539f9e2eb303539a
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-05-23 16:05:05 +0530

    a

--- Modules/pages/newadmission.php
+++ Modules/pages/newadmission.php
@@ -1,58 +1,55 @@
 <div class="container p-5">
     <h3>New Admissions</h3>
-<form>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">Name Of Student </label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
-    </div>
-    <div class="form-group col-md-6">
-      <label for="inputPassword4">Student Aadhaar No.</label>
-      <input type="text" class="form-control" id="inputPassword4" placeholder="Password">
-    </div>
-    <div class="form-group col-md-6">
-    <label for="inputAddress">Date of Birth</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-  </div>
-  <div class="form-group">
-    <label for="inputAddress2 col-md-3">Place of Birth</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-  </div>
-  <div class="form-row">
-    <div class="form-group col-md-3">
-      <label for="inputCity">District</label>
-      <input type="text" class="form-control" id="inputCity">
-    </div>
-    <div class="form-group col-md-4">
-      <label for="inputState">State</label>
-      <select id="inputState" class="form-control">
-        <option selected>Choose...</option>
-        <option>...</option>
-      </select>
-    </div>
-    <div class="form-group col-md-4">
-      <label for="inputZip">Religion</label>
-      <input type="text" class="form-control" id="inputZip">
-    </div>
-    <div class="form-group col-md-4">
-      <label for="inputZip">Caste</label>
-      <input type="text" class="form-control" id="inputZip">
-    </div>
-    <div class="form-group col-md-4">
-      <label for="inputZip">Subcaste</label>
-      <input type="text" class="form-control" id="inputZip">
-    </div>
-  </div>
- 
-  </div>
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div>
-  </div>
-  <button type="submit" class="btn btn-primary">Sign in</button>
-</form>
+    <form>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="inputEmail4">Name Of Student </label>
+                <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+            </div>
+            <div class="form-group col-md-6">
+                <label for="inputPassword4">Student Aadhaar No.</label>
+                <input type="text" class="form-control" id="inputPassword4" placeholder="Password">
+            </div>
+            <div class="form-group col-md-6">
+            <label for="inputAddress">Date of Birth</label>
+            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+        </div>
+        <div class="form-group">
+            <label for="inputAddress2 col-md-3">Place of Birth</label>
+            <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+        </div>
+        <div class="form-group col-md-3">
+                <label for="inputCity">District</label>
+                <input type="text" class="form-control" id="inputCity">
+            </div>
+            <div class="form-group col-md-4">
+                <label for="inputState">State</label>
+                <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                    <option>...</option>
+                </select>
+            </div>
+            <div class="form-group col-md-4">
+                <label for="inputZip">Religion</label>
+                <input type="text" class="form-control" id="inputZip">
+            </div>
+            <div class="form-group col-md-4">
+                <label for="inputZip">Caste</label>
+                <input type="text" class="form-control" id="inputZip">
+            </div>
+            <div class="form-group col-md-4">
+                <label for="inputZip">Subcaste</label>
+                <input type="text" class="form-control" id="inputZip">
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="gridCheck">
+                <label class="form-check-label" for="gridCheck">
+                    Check me out
+                </label>
+            </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Sign in</button>
+    </form>
 </div>
