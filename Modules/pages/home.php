73bdabe9f6491fdb2dc2e634e3dfdbd898fa6851
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-05-23 15:45:51 +0530

    Update home.php

--- Modules/pages/home.php
+++ Modules/pages/home.php
@@ -78,7 +78,7 @@
                 <div class="col-md-6 stretch-card transparent">
                   <div class="card card-light-danger">
                     <div class="card-body">
-                      <p class="mb-4">Students Absent Today</p>
+                      <p class="mb-4">New Admissions</p>
                       <p class="fs-30 mb-2">47033</p>
                       <p>0.22% (30 days)</p>
                     </div>
