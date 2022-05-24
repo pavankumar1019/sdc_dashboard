1463bf6a8ebe507dfbd3c28455a7f83ea61d03c5
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-05-24 20:20:13 +0530

    a

--- admin/Modules/pages/newadmission.php
+++ admin/Modules/pages/newadmission.php
@@ -15,18 +15,19 @@
                 <input type="date" class="form-control" id="inputAddress" >
             </div>
             
-            <div class="form-group col-md-3">
-     
+            <div class="form-group col-md-3" style="display:flex; flex-direction:column;">
+            <div class="form-check form-check-flat">
 													<label class="form-check-label">
 														<input class="checkbox" type="checkbox" name="check"  onclick="onlyOne(this)">
 													Male
 													<i class="input-helper"></i></label>
-										
+												</div>
+            <div class="form-check form-check-flat">
 													<label class="form-check-label">
 														<input class="checkbox" type="checkbox" name="check"  onclick="onlyOne(this)">
 													Female
 													<i class="input-helper"></i></label>
-											
+												</div>
 </div>
           
             <div class="form-group">
