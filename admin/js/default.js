5e3ea68b1bcc8df408d12826fe05344ec837fa03
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-05-24 20:06:58 +0530

    a

--- admin/js/default.js
+++ admin/js/default.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 $('input[type="checkbox"]').on('change', function() {
     $(this).siblings('input[type="checkbox"]').prop('checked', false);
+    alert('add;')
  });
 
 });
