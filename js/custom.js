c07ef9359db657bb99aec481057e7276f8886c2e
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-05-24 15:11:44 +0530

    a

--- js/custom.js
+++ js/custom.js
@@ -9,11 +9,5 @@ getYear();
 
 
 /** google_map js **/
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
+
 
