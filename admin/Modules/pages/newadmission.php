a79d4e8503f986810d068758d82c1c9e05c081f7
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-05-24 20:19:04 +0530

    a

--- admin/Modules/pages/newadmission.php
+++ admin/Modules/pages/newadmission.php
@@ -16,18 +16,17 @@
             </div>
             
             <div class="form-group col-md-3">
-            <div class="form-check form-check-flat">
+     
 													<label class="form-check-label">
 														<input class="checkbox" type="checkbox" name="check"  onclick="onlyOne(this)">
 													Male
 													<i class="input-helper"></i></label>
-												</div>
-            <div class="form-check form-check-flat">
+										
 													<label class="form-check-label">
 														<input class="checkbox" type="checkbox" name="check"  onclick="onlyOne(this)">
 													Female
 													<i class="input-helper"></i></label>
-												</div>
+											
 </div>
           
             <div class="form-group">
