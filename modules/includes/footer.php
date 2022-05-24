660eceb90137ebd3be624f2b8a7d6b60e8722148
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-05-24 15:12:45 +0530

    a

--- modules/includes/footer.php
+++ modules/includes/footer.php
@@ -81,7 +81,7 @@ Our Vision
         <div class="col-md-6 col-lg-3 info-col">
           <div class="map_container">
             <div class="map">
-              <div id="googleMap"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15550.794040571136!2d78.16723306977539!3d12.9911251!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bade934519b5f89%3A0x76a00ab0f2398082!2sSDC%20PU%20%26%20DEGREE%20COLLEGE%2C%20BANGARPET!5e0!3m2!1sen!2sin!4v1653385194147!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15550.794040571136!2d78.16723306977539!3d12.9911251!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bade934519b5f89%3A0x76a00ab0f2398082!2sSDC%20PU%20%26%20DEGREE%20COLLEGE%2C%20BANGARPET!5e0!3m2!1sen!2sin!4v1653385194147!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
           </div>
         </div>
