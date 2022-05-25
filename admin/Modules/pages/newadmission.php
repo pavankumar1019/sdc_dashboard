e4209c76aeb38168a9b5a81a4370e899232d43da
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-05-25 09:57:50 +0530

    a

--- admin/Modules/pages/newadmission.php
+++ admin/Modules/pages/newadmission.php
@@ -4,28 +4,28 @@
         <div class="form-row" style="font-weight:bold;">
             <div class="form-group col-md-6">
                 <label for="inputEmail4">Name Of Student </label>
-                <input type="email" class="form-control" id="inputEmail4" placeholder="Name">
+                <input type="email" class="form-control" id="inputEmail4" name="student_name" placeholder="Name" required>
             </div>
             <div class="form-group col-md-6">
                 <label for="inputPassword4">Student Aadhaar No.</label>
-                <input type="text" class="form-control" id="inputPassword4" placeholder="Aadhar No">
+                <input type="text" class="form-control" id="inputPassword4" name="student_aadhar" placeholder="Aadhar No" required>
             </div>
             <div class="form-group col-md-6">
                 <label for="inputAddress">Date of Birth</label>
-                <input type="date" class="form-control" id="inputAddress" >
+                <input type="date" class="form-control" name="dob" id="inputAddress" required>
             </div>
             
             <div class="form-group col-md-3" style="display:flex;">
             
             <div class="form-check form-check-flat">
 													<label class="form-check-label">
-														<input class="checkbox" type="checkbox" name="check"  onclick="onlyOne(this)">
+														<input class="checkbox" type="checkbox" name="check" value="m"  onclick="onlyOne(this)">
 													Male
 													<i class="input-helper"></i></label>
 												</div> &nbsp;&nbsp;&nbsp;
             <div class="form-check form-check-flat">
 													<label class="form-check-label">
-														<input class="checkbox" type="checkbox" name="check"  onclick="onlyOne(this)">
+														<input class="checkbox" type="checkbox" name="check" value="f"  onclick="onlyOne(this)">
 													Female
 													<i class="input-helper"></i></label>
 												</div>
@@ -33,13 +33,13 @@
           
             <div class="form-group">
                 <label for="inputAddress2 col-md-3">Place of Birth</label>
-                <input type="text" class="form-control" id="inputAddress2" placeholder="">
+                <input type="text" class="form-control" name="place_of_birth" id="inputAddress2" placeholder="">
             </div>
 
           
             <div class="form-group col-md-4">
                 <label for="inputState">State</label>
-                <select id="inputState" class="form-control">
+                <select id="inputState" name="state" class="form-control" required>
                 <option value="SelectState">Select State</option>
                         <option value="Andra Pradesh">Andra Pradesh</option>
                         <option value="Arunachal Pradesh">Arunachal Pradesh</option>
@@ -82,25 +82,25 @@
             </div>
             <div class="form-group col-md-2">
                 <label for="inputCity">District</label>
-                <select class="form-control" id="inputDistrict">
+                <select class="form-control" name="district" id="inputDistrict" required>
         <option value="">-- select one -- </option>
     </select>
             </div>
             <div class="form-group col-md-2">
                 <label for="inputZip">Religion</label>
-                <input type="text" class="form-control" id="inputZip">
+                <input type="text" name="religion" class="form-control" id="inputZip" required>
             </div>
             <div class="form-group col-md-2">
                 <label for="inputZip">Caste</label>
-                <input type="text" class="form-control" id="inputZip">
+                <input type="text" name="caste" class="form-control" id="inputZip" required>
             </div>
             <div class="form-group col-md-2">
                 <label for="inputZip">Subcaste</label>
-                <input type="text" class="form-control" id="inputZip">
+                <input type="text" name="subcaste" class="form-control" id="inputZip" required>
             </div>
             <div class="form-group col-md-12">
                 <label for="inputZip">Address</label>
-                <textarea name="" class="form-control" id="" cols="30" rows="3"></textarea>
+                <textarea  name="address" class="form-control" id="" cols="30" rows="3" required></textarea>
 
             </div>
             <div class="col-12">
@@ -108,12 +108,12 @@
             </div>
             <div class="form-group col-md-6">
                 <label for="inputZip">Telephone / Mobile No</label>
-                <input type="text" class="form-control" id="inputZip">
+                <input type="text" name="mobile_no" class="form-control" id="inputZip" required>
 
             </div>
             <div class="form-group col-md-6">
                 <label for="inputZip">E-mail ID</label>
-                <input type="text" class="form-control" id="inputZip">
+                <input type="text" name="email_id" class="form-control" id="inputZip">
 
             </div>
             <div class="col-12">
@@ -121,22 +121,22 @@
             </div>
             <div class="form-group col-md-6">
                 <label for="inputZip">Father Name</label>
-                <input type="text" class="form-control" id="inputZip">
+                <input type="text" name="father_name" class="form-control" id="inputZip">
 
             </div>
             <div class="form-group col-md-6">
                 <label for="inputZip">Occupation</label>
-                <input type="text" class="form-control" id="inputZip">
+                <input type="text" name="father_occ" class="form-control" id="inputZip">
 
             </div>
             <div class="form-group col-md-6">
                 <label for="inputZip">Mother Name</label>
-                <input type="text" class="form-control" id="inputZip">
+                <input type="text" name="mother_name"  class="form-control" id="inputZip">
 
             </div>
             <div class="form-group col-md-6">
                 <label for="inputZip">Occupation</label>
-                <input type="text" class="form-control" id="inputZip">
+                <input type="text" name="mother_occ" class="form-control" id="inputZip">
 
             </div>
             <div class="col-12">
@@ -144,12 +144,12 @@
             </div>
             <div class="form-group col-md-6">
                 <label for="inputZip">Name of the guardian :</label>
-                <input type="text" class="form-control" id="inputZip">
+                <input type="text" name="name_of_g" class="form-control" id="inputZip">
 
             </div>
             <div class="form-group col-md-6">
                 <label for="inputZip">Guardian address</label>
-                <input type="text" class="form-control" id="inputZip">
+                <input type="text" name="g_address" class="form-control" id="inputZip">
 
             </div>
             <div class="col-12">
@@ -157,7 +157,7 @@
             </div>
             <div class="form-group col-md-12">
                 <label for="inputZip">Parents Annual Income :</label>
-                <input type="text" class="form-control" id="inputZip">
+                <input type="text" name="annual_income" class="form-control" id="inputZip">
 
             </div>
             <div class="col-12">
@@ -165,12 +165,12 @@
             </div>
             <div class="form-group col-md-6">
                 <label for="inputZip">Name of previous school :</label>
-                <input type="text" class="form-control" id="inputZip">
+                <input type="text" name="name_of_preschool" class="form-control" id="inputZip" required>
 
             </div>
             <div class="form-group col-md-6">
                 <label for="inputZip">Address of previous school</label>
-                <input type="text" class="form-control" id="inputZip">
+                <input type="text" name="address_of_preschool" class="form-control" id="inputZip" required>
             </div>
             <div class="col-12">
                 <hr style="height:2px;border-width:0;color:gray;background-color:gray">
@@ -181,12 +181,12 @@
             <div class="form-group col-md-6">
          
                 <label for="inputZip">Reg No. :</label>
-                <input type="text" class="form-control" id="inputZip">
+                <input type="text" name="reg_no_sslc" class="form-control" id="inputZip" required>
 
             </div>
             <div class="form-group col-md-6">
                 <label for="inputZip">Year of Passing</label>
-                <input type="text" class="form-control" id="inputZip">
+                <input type="text" name="year_of_passing" class="form-control" id="inputZip" required>
             </div>
             <div class="col-12">
                 <hr style="height:2px;border-width:0;color:gray;background-color:gray">
@@ -196,7 +196,7 @@
             </div>
             <div class="form-group col-md-6">
                 <label for="inputZip">Language 1</label>
-                <select id="inputState" class="form-control">
+                <select id="inputState" name='l1' class="form-control" required>
                     <option selected>Choose...</option>
                     <option>Kannada</option>
                     <option>English</option>
@@ -206,11 +206,11 @@
             </div>
             <div class="form-group col-md-6">
                 <label for="inputZip">Marks Obtained</label>
-                <input type="text" class="form-control" id="inputZip">
+                <input type="text" name="l1_m" class="form-control" id="inputZip" required>
             </div>
             <div class="form-group col-md-6">
                 <label for="inputZip">Language 2</label>
-                <select id="inputState" class="form-control">
+                <select id="inputState" name="l2" class="form-control">
                     <option selected>Choose...</option>
                     <option>Kannada</option>
                     <option>English</option>
@@ -220,11 +220,11 @@
             </div>
             <div class="form-group col-md-6">
                 <label for="inputZip">Marks Obtained</label>
-                <input type="text" class="form-control" id="inputZip">
+                <input type="text" name="l2_m" class="form-control" id="inputZip" required>
             </div>
             <div class="form-group col-md-6">
                 <label for="inputZip">Language 3</label>
-                <select id="inputState" class="form-control">
+                <select id="inputState" name="l3" class="form-control">
                     <option selected>Choose...</option>
                     <option>Kannada</option>
                     <option>English</option>
@@ -235,40 +235,40 @@
             
             <div class="form-group col-md-6">
                 <label for="inputZip">Marks Obtained</label>
-                <input type="text" class="form-control" id="inputZip">
+                <input type="text" name="l3_m" class="form-control" id="inputZip" required>
             </div>
            
             <div class="form-group col-md-6">
-                <input type="text" class="form-control" placeholder="Science" id="inputZip" disabled>
+                <input type="text" name='s1' class="form-control" placeholder="Science" id="inputZip" disabled>
             </div>
             <div class="form-group col-md-4">
             <label for="inputZip">Marks Obtained</label>
-                <input type="text" class="form-control" placeholder="Science" id="inputZip" >
+                <input type="text" name="s1_m" class="form-control" placeholder="Science" id="inputZip" required>
             </div>
             <div class="form-group col-md-6">
-                <input type="text" class="form-control" placeholder="Maths" id="inputZip" disabled>
+                <input type="text" name="s2" class="form-control" placeholder="Maths" id="inputZip" disabled>
             </div>
             <div class="form-group col-md-4">
             <label for="inputZip"> Marks Obtained</label>
-                <input type="text" class="form-control" placeholder="Science" id="inputZip" >
+                <input type="text" name="s2_m" class="form-control" placeholder="maths" id="inputZip" required>
             </div>
             <div class="form-group col-md-6">
-                <input type="text" class="form-control" placeholder="Social Science" id="inputZip" disabled>
+                <input type="text" name="s3" class="form-control" placeholder="Social Science" id="inputZip" disabled>
             </div>
             <div class="form-group col-md-4">
             <label for="inputZip"> Marks Obtained</label>
-                <input type="text" class="form-control" placeholder="Social Science" id="inputZip" >
+                <input type="text" name="s3_m" class="form-control" placeholder="Social Science" id="inputZip" required>
             </div>
             <div class="form-group col-md-4">
-            <label for="inputZip">% of Marks Obtained</label>
-                <input type="text" class="form-control" placeholder="Science" id="inputZip" >
+            <label for="inputZip">total_marks_obtained</label>
+                <input type="text" name="marks"  class="form-control" placeholder="Science" id="inputZip" required>
             </div>
             <div class="form-group col-md-6">
            
                       <label>File upload (only img,png)</label>
                    
                       <div class="input-group col-xs-12">
-                        <input type="file" class="form-control file-upload-info" id="deploy" accept=".jpg, .png, .jpeg">
+                        <input type="file" class="form-control file-upload-info" name="img" id="deploy" accept=".jpg, .png, .jpeg" required>
                                             </div>
                                </div>
             
@@ -277,7 +277,7 @@
             </div>
             <div class="form-group col-md-6">
                 <label for="inputZip">Combination Opted</label>
-                <select id="inputState" class="form-control">
+                <select id="inputState" name="combination_opted" class="form-control" required>
                     <option selected>Choose...</option>
                     <option>PCMB</option>
                     <option>PCMCs</option>
@@ -288,7 +288,7 @@
             </div>
             <div class="form-group col-md-6">
                 <label for="inputZip">Language Opted</label>
-                <select id="inputState" class="form-control">
+                <select id="inputState" name="lang_opted" class="form-control" required>
                     <option selected>Choose...</option>
                     <option>Hindi</option>
                     <option>Kannada</option>
@@ -311,6 +311,6 @@
                 </p>
             </div>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary btn-block">Submit</button>
     </form>
 </div>
