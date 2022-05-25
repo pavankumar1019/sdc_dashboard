72fac2630188bcbc8cd105f8b5b2c97ab2f0bec9
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-05-25 21:01:24 +0530

    a

--- admin/Modules/pages/newadmissionData.php
+++ admin/Modules/pages/newadmissionData.php
@@ -1,15 +1,26 @@
-<div class="container">
-      <h3 align="center">Live Data Search with Pagination in PHP Mysql using Ajax</h3>
-      <br />
-      <div class="card">
+
+
+<div class="main-panel">
+    <div class="content-wrapper">
+        <div class="row">
+            <div class="col-md-12 grid-margin">
+            <div class="container">
+    <h3 align="center">Live Data Search with Pagination in PHP Mysql using Ajax</h3>
+    <br />
+    <div class="card">
         <div class="card-header">Dynamic Data</div>
         <div class="card-body">
-          <div class="form-group">
-            <input type="text" name="search_box" id="search_box" class="form-control" placeholder="Type your search query here" />
-          </div>
-          <div class="table-responsive" id="dynamic_content">
-            
-          </div>
+            <div class="form-group">
+                <input type="text" name="search_box" id="search_box" class="form-control"
+                    placeholder="Type your search query here" />
+            </div>
+            <div class="table-responsive" id="dynamic_content">
+
+            </div>
         </div>
-      </div>
     </div>
+</div>
+            </div>
+        </div>
+    </div>
+</div>
