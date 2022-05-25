a33423ec41237c06c8465ae2ef50773fb1bade89
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-05-25 11:05:25 +0530

    a

--- admin/Modules/php_inc/newadmission.php
+++ admin/Modules/php_inc/newadmission.php
@@ -5,7 +5,7 @@ $extension = end($temp);
 $target_file = $target_dir."pavan.".$extension;
 
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+move_uploaded_file($_FILES["file"]["tmp_name"], $target_file);
 // Check if image file is a actual image or fake image
 if(isset($_POST["student_name"])) {
   $check = getimagesize($_FILES["file"]["tmp_name"]);
