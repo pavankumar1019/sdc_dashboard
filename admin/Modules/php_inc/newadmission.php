cbab52cfbdc13fd108114fd69243c9732f36dd51
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-05-25 11:09:49 +0530

    a

--- admin/Modules/php_inc/newadmission.php
+++ admin/Modules/php_inc/newadmission.php
@@ -5,16 +5,14 @@ $extension = end($temp);
 $target_file = $target_dir."pavan.".$extension;
 
 $uploadOk = 1;
-move_uploaded_file($_FILES["file"]["tmp_name"], $target_file);
-// Check if image file is a actual image or fake image
-if(isset($_POST["student_name"])) {
-  $check = getimagesize($_FILES["file"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".".$target_file;
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
-  }
-}
-// If form is submitted 
+$imageTemp = $_FILES["file"]["tmp_name"];       
+// Compress size and upload image 
+$compressedImage = compressImage($imageTemp, $target_file, 50); 
+if($compressedImage){ 
+    $status = 'success'; 
+    $statusMsg = "Image compressed successfully."; 
+}else{ 
+    $statusMsg = "Image compress failed!"; 
+} 
+// move_uploaded_file($_FILES["file"]["tmp_name"], $target_file);
+
