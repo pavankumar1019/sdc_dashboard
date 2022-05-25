e55d3ae6bd049fd8c066089791337ebc9e8c03e8
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-05-25 12:25:53 +0530

    a

--- admin/Modules/php_inc/newadmission.php
+++ admin/Modules/php_inc/newadmission.php
@@ -41,13 +41,7 @@ $imageTemp = $_FILES["file"]["tmp_name"];
 // Compress size and upload image 
 $compressedImage = compressImage($imageTemp, $target_file, 50); 
 if($compressedImage){ 
-    $status = 'success'; 
-    $statusMsg = "Image compressed successfully."; 
-}else{ 
-    $statusMsg = "Image compress failed!"; 
-} 
-
-$subjects=$_POST['l1'].",".$_POST['l2'].",".$_POST['l3'].$_POST['Science'].",".$_POST['Maths'].",".$_POST['Social'];
+    $subjects=$_POST['l1'].",".$_POST['l2'].",".$_POST['l3'].$_POST['Science'].",".$_POST['Maths'].",".$_POST['Social'];
 $marks=$_POST['l1_m'].",".$_POST['l2_m'].",".$_POST['l3_m'].$_POST['s1_m'].",".$_POST['s2_m'].",".$_POST['s3_m'];
 $per=($_POST['marks']/625)*100;
  $insert_data = array(
@@ -80,15 +74,26 @@ $per=($_POST['marks']/625)*100;
  "marks"=>$marks,
  "total"=>$_POST['marks'],
  "%"=>$per,
- "photo"=>$_POST[14],
- "combination_opted"=>$_POST[14],
- "lang_opted"=>$_POST[14],
+ "photo"=>$_POST['target_file'],
+ "combination_opted"=>$_POST['combination_opted'],
+ "lang_opted"=>$_POST['lang_opted'],
 
  );
  $table_columns= implode(',', array_keys($insert_data));
  $table_data= implode("', '", $insert_data);
 $sql = "INSERT INTO new_admission_bpet ($table_columns) VALUES ('$table_data') "; 
-$conn->query($sql);
+if($conn->query($sql)== TRUE){
+    $status = 'success'; 
+    $statusMsg = "Image compressed successfully."; 
+}else{
+    $statusMsg="Failed to add";
+}
+  
+}else{ 
+    $statusMsg = "Image compress failed!"; 
+} 
+
+
 
 // move_uploaded_file($_FILES["file"]["tmp_name"], $target_file);
 // Check if image file is a actual image or fake image
