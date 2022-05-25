65daafc584837775d3463c3b710b42aeed301f2a
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-05-25 10:44:04 +0530

    a

--- admin/Modules/php_inc/newadmission.php
+++ admin/Modules/php_inc/newadmission.php
@@ -1,53 +1,17 @@
 <?php 
-$uploadDir = 'uploads/'; 
-$response = array( 
-    'status' => 0, 
-    'message' => 'Form submission failed, please try again.' 
-); 
- 
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["file"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["student_name"])) {
+  $check = getimagesize($_FILES["file"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
 // If form is submitted 
-if(isset($_POST['student_name']) || isset($_POST['student_aadhar']) || isset($_POST['reg_no_sslc'])){ 
-   
-            // Upload file 
-            $uploadedFile = ''; 
-            if(!empty($_FILES["file"]["name"])){ 
-                 
-                // File path config 
-                $fileName = basename($_FILES["file"]["name"]); 
-                $targetFilePath = $uploadDir . $fileName; 
-                $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION); 
-                 
-                // Allow certain file formats 
-                $allowTypes = array('pdf', 'doc', 'docx', 'jpg', 'png', 'jpeg'); 
-                if(in_array($fileType, $allowTypes)){ 
-                    // Upload file to the server 
-                    if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){ 
-                        $uploadedFile = $fileName; 
-                    }else{ 
-                        $uploadStatus = 0; 
-                        $response['message'] = 'Sorry, there was an error uploading your file.'; 
-                    } 
-                }else{ 
-                    $uploadStatus = 0; 
-                    $response['message'] = 'Sorry, only PDF, DOC, JPG, JPEG, & PNG files are allowed to upload.'; 
-                } 
-            } 
-             
-            // if($uploadStatus == 1){ 
-            //     // Include the database config file 
-            //     include_once '../../../php_include/db/db.php'; 
-                 
-            //     // Insert form data in the database 
-            //     $insert = $conn->query("INSERT INTO form_data (name,email,file_name) VALUES ('".$name."','".$email."','".$uploadedFile."')"); 
-                 
-            //     if($insert){ 
-            //         $response['status'] = 1; 
-            //         $response['message'] = 'Form data submitted successfully!'; 
-            //     } 
-            // } 
-        } 
-   
-
- 
-// Return response 
-echo json_encode($response);
