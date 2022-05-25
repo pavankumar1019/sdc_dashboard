acc11f149a4e643bb57888b51ae465e7a776a114
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-05-25 21:45:18 +0530

    a

--- admin/Modules/pages/newadmissionData.php
+++ admin/Modules/pages/newadmissionData.php
@@ -1,25 +1,22 @@
-
-
 <div class="main-panel">
     <div class="content-wrapper">
         <div class="row">
-            <div class="col-md-12 grid-margin">
-            <div class="container">
-    <h3 align="center">New Admissions 2022</h3>
-    <br />
-    <div class="card">
-        <div class="card-header">Dynamic Data</div>
-        <div class="card-body">
-            <div class="form-group">
-                <input type="text" name="search_box" id="search_box" class="form-control"
-                    placeholder="Type your search query here" />
-            </div>
-            <div class="table-responsive" id="dynamic_content">
 
-            </div>
-        </div>
-    </div>
-</div>
+            <div class="container">
+                <h3 align="center">New Admissions 2022</h3>
+                <br />
+                <div class="card">
+                    <div class="card-header">Dynamic Data</div>
+                    <div class="card-body">
+                        <div class="form-group">
+                            <input type="text" name="search_box" id="search_box" class="form-control"
+                                placeholder="Type your search query here" />
+                        </div>
+                        <div class="table-responsive" id="dynamic_content">
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
