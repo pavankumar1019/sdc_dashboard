b7bb1db0e3d0e3a16b7ccd443d19d3c9ac550958
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-05-25 16:14:02 +0530

    a

--- admin/Modules/pages/home.php
+++ admin/Modules/pages/home.php
@@ -80,7 +80,7 @@
                     <div class="card-body">
                       <p class="mb-4">New Admissions</p>
                       <p class="fs-30 mb-2" id="new_admission_count">0</p>
-                      <p>0.22% (30 days)</p>
+                      <a>View More</a>
                     </div>
                   </div>
                 </div>
