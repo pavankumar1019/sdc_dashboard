dedc2db2f0ed43c9ebd5d7cc844de3a3cd2d93d9
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-05-28 12:23:31 +0530

    a

--- admin/js/new_admission/new_admission.js
+++ admin/js/new_admission/new_admission.js
@@ -1,6 +1,23 @@
 $(document).ready(function(e){
-    $("#adhar").keyup(function(){
-      alert('asdasd');
+    $("#aadhar").keyup(function(){
+        $.ajax({
+            type: "POST",
+            data: {
+                aadhar: $(this).val(),
+            },
+            url: "./Modules/php_inc/checkdata.php",
+            success: function(data)
+            {
+                if(data === 'USER_AVAILABLE')
+                {
+                   $('#checkdata').html('Data already Present');
+                }
+                else
+                {
+                    $('#checkdata').html('');
+                }
+            }
+        })  
       });
     // Submit form data via Ajax
     $("#fupForm").on('submit', function(e){
