987849ac867b73fb9c3f995eb0d6423b74dadf26
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-06-06 13:57:26 +0530

    a

--- getdata.php
+++ getdata.php
@@ -1,6 +1,9 @@
 <?php
 
-
+header("Content-Type: application/xls");    
+header("Content-Disposition: attachment; filename=getdata.xls");  
+header("Pragma: no-cache"); 
+header("Expires: 0");
 
 $servername = "localhost";
 $username = "u430139865_sdc";
@@ -18,15 +21,11 @@ $sql = "SELECT `student_name`,`combination_opted`,`mobile_no` FROM `new_admissio
 $result=$conn->query($sql);
 echo '<table border="1">';
 //make the column headers what you want in whatever order you want
-echo '<tr><th> Name </th><th>Combination Opted</th><th>Mobile Number</th></tr>';
+echo '<tr><th>Field Name 1</th><th>Field Name 2</th><th>Field Name 3</th></tr>';
 //loop the query data to the table in same order as the headers
 foreach($result as $row){
     echo "<tr><td>".$row['student_name']."</td><td>".$row['combination_opted']."</td><td>".$row['mobile_no']."</td></tr>";
 }
 echo '</table>';
 
-header("Content-Type: application/xls");    
-header("Content-Disposition: attachment; filename=data.xls");  
-header("Pragma: no-cache"); 
-header("Expires: 0");
 ?>
