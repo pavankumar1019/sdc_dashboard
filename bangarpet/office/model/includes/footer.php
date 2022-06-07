f8938345f748f24372a5a6d36faa54def52e1fef
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-06-07 21:19:23 +0530

    a

--- bangarpet/office/model/includes/footer.php
+++ bangarpet/office/model/includes/footer.php
@@ -2,7 +2,7 @@
 	<script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/js/pcoded.min.js"></script>
 <?php
-if(isset($_GET['page']=="markascard")){
+if(isset($_GET['page'])=="markascard"){
     ?>
      <script src="assets/js/markscard.min.js"></script>
     <?php
