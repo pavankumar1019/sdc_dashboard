f499ba57ac5b959401e40ce970e6ac0dae4341b6
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-06-08 20:20:20 +0530

    a

--- bangarpet/office/assets/js/newadmission.js
+++ bangarpet/office/assets/js/newadmission.js
@@ -70,10 +70,13 @@ load_data(1);
             $('#caste').val(data.caste);
             let text = data.dob;
             const myArray = text.split("-");
-           myArray = text.split("/");
           $('#dd').val(myArray[0]);
             $('#mm').val(myArray[1]);
             $('#yy').val(myArray[2]);
+            const myArray1 = text.split("/");
+          $('#dd').val(myArray1[0]);
+            $('#mm').val(myArray1[1]);
+            $('#yy').val(myArray1[2]);
 
 
             $('#religion').append(`<option value="${data.religion}" selected> ${data.religion} </option>`);
