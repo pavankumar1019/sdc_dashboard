8eabcab99de9dd4ad8f9c28ee4b6b49efefc8b95
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-06-12 19:19:50 +0530

    a

--- bangarpet/staff/db/db.php
+++ bangarpet/staff/db/db.php
@@ -1,8 +1,9 @@
 <?php
+
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "test";
+$username = "u430139865_sdc";
+$password = "Pavan5639";
+$dbname = "u430139865_sdc";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -10,4 +11,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+$connect = new PDO("mysql:host=localhost; dbname=u430139865_sdc", "u430139865_sdc", "Pavan5639");
 ?>
