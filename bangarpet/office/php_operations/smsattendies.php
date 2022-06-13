3cded1d97b90095f43f5a5215868ffdd46db8a8a
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-06-13 16:35:26 +0530

    a

--- bangarpet/office/php_operations/smsattendies.php
+++ bangarpet/office/php_operations/smsattendies.php
@@ -67,7 +67,7 @@ if ($result->num_rows > 0) {
         </td>
         <input type="hidden" name="reg_no[]" id="reg_no" value="'.$row['RollNo'].'"/>
         <input type="hidden" name="status[]" id="status" class="'.$row['RollNo'].'" value="P"  />
-        <td><a onclick="markabsent('.$row['RollNo'].')" class="label theme-bg2 text-white f-20" style="cursor: pointer;">A</a><a  style="cursor: pointer;" onclick="markpresent('.$row['RollNo'].')" class="label theme-bg text-white f-20">P</a></td>
+        <td><a onclick="markabsent("'.$row['RollNo'].'")" class="label theme-bg2 text-white f-20" style="cursor: pointer;">A</a><a  style="cursor: pointer;" onclick="markpresent("'.$row['RollNo'].'")" class="label theme-bg text-white f-20">P</a></td>
         </tr>
         
         ';
