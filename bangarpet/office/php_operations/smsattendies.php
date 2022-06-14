f92ddfa73a3accec800b32917190d7d592eff489
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-06-14 10:27:47 +0530

    a

--- bangarpet/office/php_operations/smsattendies.php
+++ bangarpet/office/php_operations/smsattendies.php
@@ -41,7 +41,7 @@ SDC COLLEGE BANGARPET-563114');
     // Prepare data for POST request
     
     // Send the POST request with cURL
-    $ch = curl_init('https://smsforall.com/portal/receive_api/api_request?method=sendMessage&mobileno='.$stdnumber['mobile_no'].'&content='.$content.'&loginid=Sdcbpet2&auth_scheme=PLAIN&password=Sajsdc@25');
+    $ch = curl_init('https://smsforall.com/portal/receive_api/api_request?method=sendMessage&mobileno='.$stdnumber['mobile_no'].'&content='.$content.'&loginid=Sdcbpet2&auth_scheme=PLAIN&password=Sajsdc@25&senderid=SDCPUC');
     curl_setopt($ch, CURLOPT_POST, false);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $response = curl_exec($ch);
