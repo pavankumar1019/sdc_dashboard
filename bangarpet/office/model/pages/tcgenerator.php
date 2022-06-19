2d19cd532abda0de9eea5416615af0f4e6fba268
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-06-19 16:02:05 +0530

    a

--- bangarpet/office/model/pages/tcgenerator.php
+++ bangarpet/office/model/pages/tcgenerator.php
@@ -38,50 +38,27 @@
                                                 <div class="col-md-6">
 
                                                     <div class="form-group">
-                                                        <label for="exampleInputEmail1">Email address</label>
-                                                        <input type="email" class="form-control" id="exampleInputEmail1"
+                                                        <label for="exampleInputEmail1">Admission No</label>
+                                                        <input type="text" class="form-control" id="exampleInputEmail1"
                                                             aria-describedby="emailHelp" placeholder="Enter email">
                                                         <!-- <small id="emailHelp" class="form-text text-muted">We'll never
                                                             share your email with anyone else.</small> -->
                                                     </div>
-                                                    <div class="form-group">
-                                                        <label for="exampleInputPassword1">Password</label>
-                                                        <input type="password" class="form-control"
-                                                            id="exampleInputPassword1" placeholder="Password">
-                                                    </div>
-                                                    <div class="form-group form-check">
-                                                        <input type="checkbox" class="form-check-input"
-                                                            id="exampleCheck1">
-                                                        <label class="form-check-label" for="exampleCheck1">Check me
-                                                            out</label>
-                                                    </div>
-                                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                                 
 
                                                 </div>
                                                 <div class="col-md-6">
 
-                                                    <div class="form-group">
-                                                        <label>Text</label>
-                                                        <input type="text" class="form-control" placeholder="Text">
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <label for="exampleFormControlSelect1">Example select</label>
-                                                        <select class="form-control" id="exampleFormControlSelect1">
-                                                            <option>1</option>
-                                                            <option>2</option>
-                                                            <option>3</option>
-                                                            <option>4</option>
-                                                            <option>5</option>
-                                                        </select>
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <label for="exampleFormControlTextarea1">Example
-                                                            textarea</label>
+                                                <div class="form-group">
+                                                        <label for="exampleFormControlTextarea1">
+                                                        Place of birth , district</label>
                                                         <textarea class="form-control" id="exampleFormControlTextarea1"
                                                             rows="3"></textarea>
                                                     </div>
+                                                 
 
                                                 </div>
+                                               
                                             </div>
                                         </form>
                                     </div>
