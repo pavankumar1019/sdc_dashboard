078513f98ddbec48f465bb539df1ca65c8004510
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-06-19 19:20:42 +0530

    a

--- bangarpet/office/model/pages/tcgenerator.php
+++ bangarpet/office/model/pages/tcgenerator.php
@@ -30,10 +30,6 @@
                                     <div class="card-header">
 <h5><?php
 require_once('./db_bpet_sdc/db.php');
-
-$sql = "INSERT INTO tc (`name`)
-VALUES (' ')";
-
 if (mysqli_query($conn, $sql)) {
   $last_id = mysqli_insert_id($conn);
   echo "New record created successfully. Last inserted ID is: " . $last_id;
