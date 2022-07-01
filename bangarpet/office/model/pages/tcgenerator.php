7ba76bfccf30d752964f31dbe850ef4a7845babd
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-01 12:23:33 +0530

    Update tcgenerator.php

--- bangarpet/office/model/pages/tcgenerator.php
+++ bangarpet/office/model/pages/tcgenerator.php
@@ -84,8 +84,9 @@ $id=($row['id']+1);
                                                 <div class="col-md-2">
                                                     <div class="form-group">
                                                         <label for="exampleInputEmail1">DOB</label>
-                                                        <input type="text" class="form-control" id="exampleInputEmail1"
-                                                            aria-describedby="emailHelp" name="t20" placeholder="" required>
+                                                        <textarea type="text" class="form-control" id="exampleInputEmail1"
+                                                            aria-describedby="emailHelp" name="t20" placeholder="" required></textarea>
+                                                    
                                                     </div>
                                                 </div>
                                                 <div class="col-md-2">
