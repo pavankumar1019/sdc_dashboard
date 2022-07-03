aef0dda2af03436ce8fd505a28356bf3528a695c
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-03 20:26:18 +0530

    a

--- bangarpet/office/dashboard.php
+++ bangarpet/office/dashboard.php
@@ -3,11 +3,11 @@ session_start();
 
 if(isset($_SESSION['name']))
 {
-    if($_SESSION['branch']=1){
+    if($_SESSION['branch']==1){
         // bangarpet branch tabels
         $sdc_marks_card="sdc_marks_card_bpet"; 
      }
-if($_SESSION['branch']=2){
+if($_SESSION['branch']==2){
     // kgf branch tabels
     $sdc_marks_card="sdc_marks_card_kgf"; 
 }
