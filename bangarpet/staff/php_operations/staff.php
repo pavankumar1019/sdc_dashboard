2660e6280f08a876bd14fb1611a9cc258546621a
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-05 11:00:53 +0530

    a

--- bangarpet/staff/php_operations/staff.php
+++ bangarpet/staff/php_operations/staff.php
@@ -1,26 +1,19 @@
 <?php
-$valid_extensions = array('jpeg', 'jpg','png'); 
+$img_path = $_FILES['file']['name'];
+ 
+// get the extension of the image to generate base64 string
+$type = pathinfo($img_path, PATHINFO_EXTENSION);
+ 
+// get the file data
+$img_data = file_get_contents($img_path);
+ 
+// get base64 encoded code of the image
+$base64_code = base64_encode($img_data);
+ 
+// create base64 string of image
+$base64_str = 'data:image/' . $type . ';base64,' . $base64_code;
 
-if ( 0 < $_FILES['file']['error'] ) {
-    echo 'Error: ' . $_FILES['file']['error'] . '<br>';
-}
-else {
-   
-    $code=mt_rand(10,100000);/* rename the file name*/
-    $size= $_FILES['file']['size'];
-    $ext = strtolower(pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION));
-    if($size > 2097152) /*2 mb 1024*1024 bytes*/
-    {
-        echo json_encode(array("statusCode"=>400,'msg'=>"Image allowd less than 2 mb"));
-    }
-    else if(!in_array($ext, $valid_extensions)) {
-        echo json_encode(array("statusCode"=>400,'msg'=>$ext.' not allowed'));
-    }
-    else{
-       
-        $result = move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/' . $code.'.'.$ext);
-        echo json_encode(array("statusCode"=>200 ,'code'=>$code));
-    }
+echo json_encode(array("statusCode"=>200 ,'code'=>$base64_str));
     
-}
+
 ?>
