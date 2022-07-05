532b4245447dee74806194deb3b17c88470005ad
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-05 12:22:04 +0530

    a

--- bangarpet/staff/php_operations/staff.php
+++ bangarpet/staff/php_operations/staff.php
@@ -9,9 +9,22 @@ if($_POST['type']=="enroll"){
 if ($result->num_rows > 0) {
     echo json_encode(array("statusCode"=>201,'code'=>'Already Enrolled'));
 }else{
+    // convert to base 64 
     $bin_string = file_get_contents($_FILES["file"]["tmp_name"]);
     $base64 = 'data:image/' . $type . ';base64,' . base64_encode($bin_string);
-    echo json_encode(array("statusCode"=>200,'code'=>$base64));
+   $image= json_encode(array("statusCode"=>200,'code'=>$base64));
+
+
+    $sqlinsert = "INSERT INTO `calendar`( `name`, `phone_no`, `photo`, `branch`) 
+	VALUES ('".$_POST['name']."', '".$_POST['phone']."', '$image', '".$_POST['branch']."' )";
+if($conn->query($sqlinsert)==TRUE){
+  
+}
+else{
+    echo json_encode(array("statusCode"=>202,'code'=>'failed to enroll'));
+
+}
+  
 }
    
 }
