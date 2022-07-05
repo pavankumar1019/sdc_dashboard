126bed8d74c6786ea3617722467587789fe88e2f
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-05 11:26:12 +0530

    a

--- bangarpet/staff/php_operations/staff.php
+++ bangarpet/staff/php_operations/staff.php
@@ -4,7 +4,7 @@ $type = pathinfo($path, PATHINFO_EXTENSION);
 $data = file_get_contents($path);
 $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
 
-echo $data;
+echo $base64;
 
 
 
