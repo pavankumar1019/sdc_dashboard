9f8e1b0d7634d99073689cd5e10990138e82b1a3
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-05 11:47:07 +0530

    A

--- bangarpet/staff/php_operations/staff.php
+++ bangarpet/staff/php_operations/staff.php
@@ -1,10 +1,8 @@
 <?php
-$bin_string = file_get_contents($_FILES["file"]["tmp_name"]);
-$base64 = 'data:image/' . $type . ';base64,' . base64_encode($bin_string);
 
-echo $base64;
-
-echo $_FILES["file"]["name"];
-
-
+if($_POST['type']=="enroll"){
+    $bin_string = file_get_contents($_FILES["file"]["tmp_name"]);
+    $base64 = 'data:image/' . $type . ';base64,' . base64_encode($bin_string);
+    echo json_encode(array("statusCode"=>200));
+}
 ?>
