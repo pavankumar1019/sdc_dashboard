e7a6b8505592e6ad934e3c0f4cc697bca8b9aa31
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-06 09:51:12 +0530

    a

--- bangarpet/office/model/pages/tcgenerator.php
+++ bangarpet/office/model/pages/tcgenerator.php
@@ -230,6 +230,34 @@ $id=($row['id']+1);
                                                     <div class="form-group">
                                                     <button type="submit" class="btn btn-primary">GENERATE</button>                                                    </div>
                                                 </div>
+                   
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+ View TC
+</button>                                                </div>                             <!-- Button trigger modal -->
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
                                             </div>
                                         </form>
                                     </div>
