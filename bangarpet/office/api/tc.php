402134548b7ac2ccb8a3e25e037dc4723fd38504
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-06 09:29:58 +0530

    a

--- bangarpet/office/api/tc.php
+++ bangarpet/office/api/tc.php
@@ -1,15 +1,9 @@
 <?php
 include('../db_bpet_sdc/db.php');
 
-$sql = "INSERT INTO tc (`name`)
-VALUES (".$_POST['id'].")";
+$sql = "INSERT INTO tc (`name`) VALUES ('".$_POST['id']."')";
+mysqli_query($conn, $sql);
 
-if (mysqli_query($conn, $sql)) {
-//   $last_id = mysqli_insert_id($conn);
-//   echo "New record created successfully. Last inserted ID is: " . $last_id;
-} else {
-//   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
 
 function translate_names($num)
 { 
