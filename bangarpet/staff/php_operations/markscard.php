568f4c74121d499e624a58d3918ed270aed1e01e
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-06 14:07:01 +0530

    a

--- bangarpet/staff/php_operations/markscard.php
+++ bangarpet/staff/php_operations/markscard.php
@@ -347,22 +347,14 @@ if($_POST['type']=="edit"){
 if($_POST['type']=="update"){
 
   $insert_data = array(
-    "sats_no"=>$_POST['sats_no'],
-    "student_no"=>$_POST['student_no'],
-    "reg_no"=>$_POST['reg_no'],
-     "student_name"=>$_POST['name'],
-     "father_name"=>$_POST['father_name'],
-     "mother_name"=>$_POST['mother_name'],
-     "langcode"=>$_POST['lang1'],
-     "l1"=>$_POST['l1'],
-     "l2"=>$_POST['l2'],
-     "s1"=>$_POST['s1'],
-     "s2"=>$_POST['s2'],
-     "s3"=>$_POST['s3'],
-     "s4"=>$_POST['s4'],
-     "gt"=>$_POST['gt'],
-     "class_code"=>$_POST['combination_opted'],
-     "year_of_passing"=>$_POST['year_of_passing']
+    "roll"=>$_POST['reg_no'],
+    "l1"=>$_POST['l1'],
+    "l2"=>$_POST['l2'],
+    "s1"=>$_POST['s1'],
+    "s2"=>$_POST['s2'],
+    "s3"=>$_POST['s3'],
+    "s4"=>$_POST['s4'],
+    "total"=>$_POST['gt'],
      );
   $query = '';   
   foreach($insert_data as $key => $value)  
