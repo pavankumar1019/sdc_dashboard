75d28636c8ea8b549cd6604093b26ae06a6a6462
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-08 11:33:26 +0530

    'a'

--- bangarpet/staff/model/pages/downloadconsolidate.php
+++ bangarpet/staff/model/pages/downloadconsolidate.php
@@ -27,7 +27,7 @@
                             <div class="col-sm-12">
                                 <div class="card">
                                     <div class="card-header">
-                                        <h5>Requests</h5>
+                                        <h5>Download</h5>
                                     </div>
                                     <div class="card-block">
 
