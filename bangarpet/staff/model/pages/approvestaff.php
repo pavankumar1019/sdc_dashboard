a09620b1b5a3a4ed0f3e395cc7a01925717355c0
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-08 09:56:06 +0530

    a

--- bangarpet/staff/model/pages/approvestaff.php
+++ bangarpet/staff/model/pages/approvestaff.php
@@ -10,22 +10,24 @@ include('./db_bpet_sdc/db.php');
 </button> -->
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Assign Class</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      <form>
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+        <form>
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Assign Class</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
 
-  <div class="form-group">
-    <label for="exampleFormControlSelect1">Assign Class</label>
-    <select class="form-control" id="exampleFormControlSelect1">
-   <?php
+
+                <div class="form-group">
+                    <label for="exampleFormControlSelect1">Assign Class</label>
+                    <select class="form-control" id="exampleFormControlSelect1">
+                        <?php
    $sql="SELECT * FROM class";
 $result=$conn->query($sql);
 foreach($result as $row){
@@ -33,62 +35,66 @@ foreach($result as $row){
 }
 
    ?>
-    </select>
-  </div>
-</form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Accept</button>
-      </div>
+                    </select>
+                </div>
+                <input type="text" name="id" id="id">
+                <input type="text" name="type" id="type">
+              
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Accept</button>
+            </div>
+            </form>
+        </div>
     </div>
-  </div>
 </div>
 <div class="pcoded-main-container">
-        <div class="pcoded-wrapper">
-            <div class="pcoded-content">
-                <div class="pcoded-inner-content">
-                    <!-- [ breadcrumb ] start -->
-                    <div class="page-header">
-                        <div class="page-block">
-                            <div class="row align-items-center">
-                                <div class="col-md-12">
-                                    <div class="page-header-title">
-                                        <h5 class="m-b-10">Approve Staff</h5>
-                                    </div>
-                                    <ul class="breadcrumb">
-                                        <li class="breadcrumb-item"><a href="./dashboard.php?page=home"><i class="feather icon-home"></i></a></li>
-                                        <li class="breadcrumb-item"><a href="javascript:">approve staff</a></li>
-                                    </ul>
+    <div class="pcoded-wrapper">
+        <div class="pcoded-content">
+            <div class="pcoded-inner-content">
+                <!-- [ breadcrumb ] start -->
+                <div class="page-header">
+                    <div class="page-block">
+                        <div class="row align-items-center">
+                            <div class="col-md-12">
+                                <div class="page-header-title">
+                                    <h5 class="m-b-10">Approve Staff</h5>
                                 </div>
+                                <ul class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="./dashboard.php?page=home"><i
+                                                class="feather icon-home"></i></a></li>
+                                    <li class="breadcrumb-item"><a href="javascript:">approve staff</a></li>
+                                </ul>
                             </div>
                         </div>
                     </div>
-                    <!-- [ breadcrumb ] end -->
-                    <div class="main-body">
-                        <div class="page-wrapper">
-                            <!-- [ Main Content ] start -->
-                            <div class="row">
-                                <div class="col-sm-12">
-                                    <div class="card">
-                                        <div class="card-header">
-                                            <h5>Requests</h5>
-                                        </div>
-                                        <div class="card-block" id="dynamic_content">
-                                      
-                                         
+                </div>
+                <!-- [ breadcrumb ] end -->
+                <div class="main-body">
+                    <div class="page-wrapper">
+                        <!-- [ Main Content ] start -->
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h5>Requests</h5>
+                                    </div>
+                                    <div class="card-block" id="dynamic_content">
 
-                                        </div>
+
+
                                     </div>
                                 </div>
                             </div>
-                            <!-- [ Main Content ] end -->
                         </div>
+                        <!-- [ Main Content ] end -->
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
 
 <?php
