18df3685c0b31913b516006a0bcd0455cea81910
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-08 12:09:16 +0530

    A

--- bangarpet/staff/php_operations/approvestaff.php
+++ bangarpet/staff/php_operations/approvestaff.php
@@ -275,7 +275,8 @@ $result=$conn->query($getdata);
 foreach($result as $row){
   $u_key = substr($row['phone_no'], -4);
   $user_id = substr($row['name'], 0, 5).$u_key;
-  
+
+
 
   $insert_data = array(
  
@@ -300,11 +301,32 @@ foreach($result as $row){
 
 
   $sql ="UPDATE `staff` SET ".$query." WHERE id='".$_POST['id']."'";  
-  if ($conn->query($sql) === TRUE) {
-    echo "Record updated successfully";
+    if ($conn->query($sql) === TRUE) {
+    $method = 'sendMessage';
+
+    // Message details
+  
+$content =  rawurlencode('Dear '.$row['name'].' 
+Your Credentials to login into staff portal User_ID:'.$user_id.' Key:'.$key.'
+SDC COLLEGE BANGARPET-563114');
+  
+  
+   
+    // Prepare data for POST request
+   
+    // Send the POST request with cURL
+    $ch = curl_init('https://smsforall.com/portal/receive_api/api_request?method=sendMessage&mobileno='.$_POST['phone_no'].'&content='.$content.'&loginid=Sdcbpet2&auth_scheme=PLAIN&password=Sajsdc@25');
+    curl_setopt($ch, CURLOPT_POST, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    echo "Done";
+
   } else {
-    echo "Error updating record: " . $conn->error;
+    echo "Failed Duplicate entry. !";
   }
+
+
 }
 }
 ?>
