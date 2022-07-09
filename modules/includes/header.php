78f2a02c758a027e8d9499a2182a0c370634ded4
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-09 11:36:32 +0530

    a

--- modules/includes/header.php
+++ modules/includes/header.php
@@ -26,10 +26,32 @@
   <link href="css/style.css" rel="stylesheet" />
   <!-- responsive style -->
   <link href="css/responsive.css" rel="stylesheet" />
-  <link rel="stylesheet" type="text/css" href="./css/addtohomescreen.css">
-<script src="./js/addtohomescreen.js"></script>
+
 <script>
-addToHomescreen();
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = 'block';
+
+  addBtn.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    addBtn.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+});
 </script>
 </head>
 
