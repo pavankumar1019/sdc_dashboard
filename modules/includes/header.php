38274d7684ed77a491a47a42889b56ea878e2f1c
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-09 11:47:37 +0530

    a

--- modules/includes/header.php
+++ modules/includes/header.php
@@ -37,7 +37,7 @@
 <script>
   let deferredPrompt;
 const addBtn = document.querySelector('.add-button');
-addBtn.style.display = 'none';
+// addBtn.style.display = 'none';
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
