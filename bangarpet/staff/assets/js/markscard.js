f052d08ad42abc5901648b04028467bccbc60ed4
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-14 20:00:11 +0530

    a

--- bangarpet/staff/assets/js/markscard.js
+++ bangarpet/staff/assets/js/markscard.js
@@ -165,7 +165,15 @@ function countdata()
             $("#gt").val(sum);
             $("#gt1").html(sum);
         });
-        
+        $('#newadmissionmodal').on('hidden.bs.modal', function (e) {
+          $(this)
+            .find("input,textarea,select")
+               .val('')
+               .end()
+            .find("input[type=checkbox], input[type=radio]")
+               .prop("checked", "")
+               .end();
+        })
     
   });
   
