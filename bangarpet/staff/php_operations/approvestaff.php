d0609f6e0e1a14d16ddf6f5b2bda05c7b944dde8
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-14 20:24:45 +0530

    A

--- bangarpet/staff/php_operations/approvestaff.php
+++ bangarpet/staff/php_operations/approvestaff.php
@@ -24,7 +24,7 @@ if($_POST['type']=="loaddata"){
       $start = 0;
     }
     
-    $query = "SELECT * FROM `staff` WHERE branch='".$_SESSION['branch_staff']."' AND role=''
+    $query = "SELECT * FROM `staff` WHERE branch='".$_SESSION['branch_staff']."' 
     ";
     
     if($_POST['query'] != '')
