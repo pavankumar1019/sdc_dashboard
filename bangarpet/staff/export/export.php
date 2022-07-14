4f1ddeda82849c902928fe33819befdb72f9c9e4
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-14 12:43:41 +0530

    a

--- bangarpet/staff/export/export.php
+++ bangarpet/staff/export/export.php
@@ -6,7 +6,7 @@ $output = '';
 // export pdf
 if($_GET["data"]=="consolidate_pdf")
 {
-     $query = "SELECT ".$tbl_admission.".StudentName,  ".$tbl_admission.".father_name, ".$tbl_admission.".mobile_no, ".$tbl_admission.".RollNo, ".$class_test_marks.".l1, ".$class_test_marks.".l2,  ".$class_test_marks.".s1, ".$class_test_marks.".s2,  ".$class_test_marks.".s3,  ".$class_test_marks.".s4,  ".$class_test_marks.".total
+     $query = "SELECT ".$tbl_admission.".StudentName,  ".$tbl_admission.".father_name, ".$tbl_admission.".lang_code, ".$tbl_admission.".mobile_no, ".$tbl_admission.".RollNo, ".$class_test_marks.".l1, ".$class_test_marks.".l2,  ".$class_test_marks.".s1, ".$class_test_marks.".s2,  ".$class_test_marks.".s3,  ".$class_test_marks.".s4,  ".$class_test_marks.".total
      FROM ".$tbl_admission."
      LEFT JOIN ".$class_test_marks." ON ".$tbl_admission.".RollNo = ".$class_test_marks.".roll AND ".$class_test_marks.".test_id=". $_SESSION['test_name']."    WHERE ".$tbl_admission.".Class=".$_SESSION['class_id']."  
      ";
@@ -20,19 +20,34 @@ if($_GET["data"]=="consolidate_pdf")
    </tr> 
                     <tr> 
                    
+                    <th>Reg_no</th>
+                   
                     <th>StudentName</th>
                     <th>father_name</th>
-                
+                    <th>Language</th>
+                    <th>Language-1</th>
+                    <th>English</th>
+                    <th>Subject-1</th>
+                    <th>Subject-2</th>
+                    <th>Subject-3</th>
+                    <th>subject-4</th>
+                    <th>Total</th>
+                    <th>%</th>
                     </tr>
   ';
   while($row = mysqli_fetch_array($result))
   {
    $output .= '
     <tr>  
+                         <td>'.$row["RollNo"].'</td>  
                          <td>'.$row["StudentName"].'</td>  
                          <td>'.$row["father_name"].'</td>  
-                     
-   
+                         <td>'.$row["lang_code"].'</td>  
+                         <td>'.$row["s1"].'</td>  
+                         <td>'.$row["s2"].'</td>  
+                         <td>'.$row["s3"].'</td>  
+                         <td>'.$row["s4"].'</td>  
+                         <td>'.$row["total"].'</td>  
                     </tr>
    ';
   }
