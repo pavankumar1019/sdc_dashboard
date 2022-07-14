827840f01e789a09f8f7c1f0eb306a0f195d7ae6
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-14 12:58:45 +0530

    A

--- bangarpet/staff/export/export.php
+++ bangarpet/staff/export/export.php
@@ -3,6 +3,25 @@ include '../db_bpet_sdc/db.php';
 require ('../../../vendor/autoload.php');
 // export to pdf
 $output = '';
+$output .= '
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+';
 // export pdf
 if($_GET["data"]=="consolidate_pdf")
 {
