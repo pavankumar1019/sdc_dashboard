b85a71d39bb89d6122502885f38b3f154ca71058
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-14 15:29:18 +0530

    a

--- bangarpet/staff/model/includes/header.php
+++ bangarpet/staff/model/includes/header.php
@@ -72,7 +72,7 @@
                     
                 <?php
                 if($_SESSION['role_staff']=="ct"){
-                    echo '<li data-username="Sample Page" class="nav-item"><a href="./dashboard.php?page=consolidate" class="nav-link"><span class="pcoded-micon"><i class="feather icon-file-text"></i></span><span class="pcoded-mtext">Upload Consolidate</span></a></li>';
+                    echo '<li data-username="Sample Page" class="nav-item"><a href="./dashboard.php?page=consolidate" class="nav-link"><span class="pcoded-micon"><i class="feather icon-file-text"></i></span><span class="pcoded-mtext">Upload Marks</span></a></li>';
                 }
                 ?>
                 <?php
