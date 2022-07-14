c311dba06bf22d024e82bc08852464177bcfa897
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-14 12:23:54 +0530

    A

--- bangarpet/staff/assets/js/script.js
+++ bangarpet/staff/assets/js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
 $('#consolidate').click(function(){
-    var page = "./modules/exports/books_exports.php?data=consolidate_pdf";  
+    var page = "./exports/export.php?data=consolidate_pdf";  
     window.location = page;  
    });
-   
+
 
 });
