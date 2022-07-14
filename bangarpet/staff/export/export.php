719928c9a8cf4a238e0529582a962f78bf5b1b07
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-14 12:27:37 +0530

    a

--- bangarpet/staff/export/export.php
+++ bangarpet/staff/export/export.php
@@ -6,63 +6,28 @@ $output = '';
 // export pdf
 if($_GET["data"]=="consolidate_pdf")
 {
- $query = "SELECT * FROM books";
+     $query = "SELECT ".$tbl_admission.".StudentName,  ".$tbl_admission.".father_name, ".$tbl_admission.".mobile_no, ".$tbl_admission.".RollNo, ".$class_test_marks.".l1, ".$class_test_marks.".l2,  ".$class_test_marks.".s1, ".$class_test_marks.".s2,  ".$class_test_marks.".s3,  ".$class_test_marks.".s4,  ".$class_test_marks.".total
+     FROM ".$tbl_admission."
+     LEFT JOIN ".$class_test_marks." ON ".$tbl_admission.".RollNo = ".$class_test_marks.".roll AND ".$class_test_marks.".test_id=". $_SESSION['test_name']."    WHERE ".$tbl_admission.".Class=".$_SESSION['class_id']."  
+     ";
  $result = mysqli_query($connect, $query);
  if(mysqli_num_rows($result) > 0)
  {
   $output .= '
    <table class="table" bordered="1">  
                     <tr>  
-                    <th>Categories</th>
-                    <th>Acession_No</th>
-                    <th>ISBN/ISSN</th>
-                    <th>Title</th>
-                    <th>Pages</th>
-                    <th>Price</th>
-                    <th>Invoice_No</th>
-                    <th>Purchase_Date</th>
-                    <th>Rack_ID</th>
-                    <th>Main_Author</th>
-                    <th>Joint_Author</th>
-                    <th>Publisher</th>
-                    <th>Place Of Publication</th>
-                    <th>Year Of Publication</th>
-                    <th>Edition</th>
-                    <th>DDCN</th>
-                    <th>Series</th>
-                    <th>Volume</th>
-                    <th>Department</th>
-                    <th>Color</th>
-                    <th>Note</th>
-                    <th>types</th>
+                    <th>StudentName</th>
+                    <th>father_name</th>
+                
                     </tr>
   ';
   while($row = mysqli_fetch_array($result))
   {
    $output .= '
     <tr>  
-                         <td>'.$row["categories"].'</td>  
-                         <td>'.$row["acession_no"].'</td>  
-                         <td>'.$row["isbn_issn_no"].'</td>  
-                         <td>'.$row["title"].'</td>  
-                         <td>'.$row["pages"].'</td>  
-                         <td>'.$row["price"].'</td>  
-                         <td>'.$row["invoice_no"].'</td>  
-                         <td>'.$row["purchase_date"].'</td>  
-                         <td>'.$row["rack_id"].'</td>  
-                         <td>'.$row["main_author"].'</td>  
-                         <td>'.$row["joint_author"].'</td>  
-                         <td>'.$row["publisher"].'</td>  
-                         <td>'.$row["place_publication"].'</td>  
-                         <td>'.$row["year_publication"].'</td>  
-                         <td>'.$row["edition"].'</td>  
-                         <td>'.$row["ddcn"].'</td>  
-                         <td>'.$row["series"].'</td>  
-                         <td>'.$row["volume"].'</td>  
-                         <td>'.$row["department"].'</td>  
-                         <td>'.$row["color"].'</td>  
-                         <td>'.$row["note"].'</td>  
-                         <td>'.$row["types"].'</td>  
+                         <td>'.$row["StudentName"].'</td>  
+                         <td>'.$row["father_name"].'</td>  
+                     
    
                     </tr>
    ';
