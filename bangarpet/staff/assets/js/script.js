558cd2f85ef30d67533cbd48f1a03e7c4ea42dc0
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-18 09:42:07 +0530

    a
    a

--- bangarpet/staff/assets/js/script.js
+++ bangarpet/staff/assets/js/script.js
@@ -9,5 +9,10 @@ $('#consolidatep').click(function(){
     var page = "./export/pexport.php?data=consolidate_pdf&class_id="+value;  
     window.location = page;  
    });
+$('#sendsms').click(function(){
+    var value=$('#consolidateclass').val();
+    var page = "./export/sendsms.php?data=consolidate_pdf&class_id="+value;  
+    window.location = page;  
+   });
 
 });
---
+++ bangarpet/staff/export/sendsms.php
@@ -0,0 +1,4 @@
+<?php
+echo $_GET['class_id']
+
+?>
