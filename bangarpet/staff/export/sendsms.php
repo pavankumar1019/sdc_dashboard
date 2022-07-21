7b0beea380eb51dd3c8f567e2bd55c24acc65a67
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-21 12:12:59 +0530

    a

--- bangarpet/staff/export/sendsms.php
+++ bangarpet/staff/export/sendsms.php
@@ -1,6 +1,11 @@
 <?php
+include '../db_bpet_sdc/db.php';
 // echo $_GET['class_id']
-
+$query = "SELECT ".$tbl_admission.".StudentName,  ".$tbl_admission.".father_name, ".$tbl_admission.".combination, ".$tbl_admission.".lang_code, ".$tbl_admission.".mobile_no, ".$tbl_admission.".RollNo, ".$class_test_marks.".l1, ".$class_test_marks.".l2,  ".$class_test_marks.".s1, ".$class_test_marks.".s2,  ".$class_test_marks.".s3,  ".$class_test_marks.".s4,  ".$class_test_marks.".total
+FROM ".$tbl_admission."
+LEFT JOIN ".$class_test_marks." ON ".$tbl_admission.".RollNo = ".$class_test_marks.".roll AND ".$class_test_marks.".test_id=". $_SESSION['test_name']."    WHERE ".$tbl_admission.".Class=".$_GET['class_id']."  
+";
+$result = mysqli_query($conn, $query);
 ?>
 <!DOCTYPE html>
 <html>
@@ -29,7 +34,7 @@ tr:nth-child(even){background-color: #f2f2f2}
 <section><div class="container">
 <div style="overflow-x:auto;">
   <table>
-    <tr>
+  <tr>
       <th>Name</th>
       <th>Phone Number</th>
       <th>L1</th>
@@ -40,42 +45,25 @@ tr:nth-child(even){background-color: #f2f2f2}
       <th>S4</th>
       <th>Total</th>
     </tr>
-    <tr>
-      <td>Jill</td>
-      <td>Smith</td>
-      <td>50</td>
-      <td>50</td>
-      <td>50</td>
-      <td>50</td>
-      <td>50</td>
-      <td>50</td>
-      <td>50</td>
+    <?php
+    foreach($result as $row){
+      echo '
+      <tr>
+      <td>'.$row['StudentName'].'</td>
+      <td>'.$row['mobile_no'].'</td>
+      <td>'.$row['l1'].'</td>
+      <td>'.$row['l2'].'</td>
+      <td>'.$row['s1'].'</td>
+      <td>'.$row['s2'].'</td>
+      <td>'.$row['s3'].'</td>
+      <td>'.$row['s4'].'</td>
+      <td>'.$row['total'].'</td>
     
     </tr>
-    <tr>
-      <td>Eve</td>
-      <td>Jackson</td>
-      <td>94</td>
-      <td>94</td>
-      <td>94</td>
-      <td>94</td>
-      <td>94</td>
-      <td>94</td>
-      <td>94</td>
+      '
+    }    ?>
     
-    </tr>
-    <tr>
-      <td>Adam</td>
-      <td>Johnson</td>
-      <td>67</td>
-      <td>67</td>
-      <td>67</td>
-      <td>67</td>
-      <td>67</td>
-      <td>67</td>
-      <td>67</td>
-  
-    </tr>
+   
   </table>
 </div>
 </div>
