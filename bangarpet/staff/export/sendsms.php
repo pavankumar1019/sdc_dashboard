b21cb54b2f8602ca752c577c366ac2c9c8e0f73f
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-21 12:56:46 +0530

    a

--- bangarpet/staff/export/sendsms.php
+++ bangarpet/staff/export/sendsms.php
@@ -10,7 +10,10 @@ $result = mysqli_query($conn, $query);
 <!DOCTYPE html>
 <html>
 <head>
+  
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
 <style>
 table {
   border-collapse: collapse;
