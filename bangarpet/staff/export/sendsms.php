0cb6a060df036ea376241729ac66e88f3d970e20
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-21 12:34:16 +0530

    a

--- bangarpet/staff/export/sendsms.php
+++ bangarpet/staff/export/sendsms.php
@@ -71,7 +71,7 @@ tr:nth-child(even){background-color: #f2f2f2}
 <div class="container">
   <div class="row">
     <div class="col text-center">
-    <form action="#"  method="post">
+    <form action=""  method="post">
       <button  name="test" class="btn btn-default bg-warning">Run SMS Server</button>
   </form>
     </div>
