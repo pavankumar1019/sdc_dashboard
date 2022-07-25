e94a2e7d4b3fb99acd3526fadd3f7a0aeca27e11
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-25 12:48:48 +0530

    A

--- result.php
+++ result.php
@@ -12,7 +12,7 @@ if ($conn->connect_error) {
 }
 $query = "SELECT tbl_admission.StudentName,  tbl_admission.father_name, tbl_admission.combination, tbl_admission.lang_code, tbl_admission.mobile_no, tbl_admission.RollNo, class_test_marks_bpet.l1, class_test_marks_bpet.l2,  class_test_marks_bpet.s1, class_test_marks_bpet.s2,  class_test_marks_bpet.s3,  class_test_marks_bpet.s4,  class_test_marks_bpet.total
 FROM tbl_admission
-LEFT JOIN class_test_marks_bpet ON tbl_admission.RollNo = class_test_marks_bpet.roll AND class_test_marks_bpet.test_id=1 WHERE tbl_admission.RollNo=".$_POST['reg']."  
+LEFT JOIN class_test_marks_bpet ON tbl_admission.RollNo = class_test_marks_bpet.roll AND class_test_marks_bpet.test_id=1 AND tbl_admission.RollNo=".$_POST['reg']."  
 ";
 $result = mysqli_query($conn, $query);
 
