d1b9fc835ed2c43b1c4b181258b93f4048ddfa07
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-25 12:53:57 +0530

    A

--- result.php
+++ result.php
@@ -7,9 +7,7 @@ $dbname = "u430139865_sdc";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+
 $query = "SELECT tbl_admission.StudentName,  tbl_admission.father_name, tbl_admission.combination, tbl_admission.lang_code, tbl_admission.mobile_no, tbl_admission.RollNo, class_test_marks_bpet.l1, class_test_marks_bpet.l2,  class_test_marks_bpet.s1, class_test_marks_bpet.s2,  class_test_marks_bpet.s3,  class_test_marks_bpet.s4,  class_test_marks_bpet.total
 FROM tbl_admission
 LEFT JOIN class_test_marks_bpet ON tbl_admission.RollNo = class_test_marks_bpet.roll AND class_test_marks_bpet.test_id=1 WHERE tbl_admission.RollNo='".$_POST['RollNo']."' 
