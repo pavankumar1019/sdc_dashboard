5e9e1a36d96ca4b383e56352b8719b8efb37f3cb
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-25 13:52:48 +0530

    a

--- sms.php
+++ sms.php
@@ -1,18 +1,19 @@
 <?php
-   $method = 'sendMessage';
-	
+   $apiKey = urlencode('NDM1OTMzNTA0MjUyMzk2MzVhNGUzMDQ4NzY3NTM5Njc=');
    // Message details
-$content =  rawurlencode('Dear PAVAN KUMAR S 
-your score in Term Test 1 KA=20/50, ENG=30/50, PHY=40/50, CHE=60/50, MAT=50/50, CS=60/50, Total 400/500.
+   $numbers = array(7483737698);
+   $sender = urlencode('SDCPUC');
+   $message = rawurlencode('Dear Hello 
+your score in Term Test 1 
 SDC COLLEGE BANGARPET-563114');
-         
-   
-   
-   // Prepare data for POST request
-   
+    
+   $numbers = implode(',', $numbers);
+    // Prepare data for POST request
+   $data = array('apikey' => $apiKey, 'numbers' => $numbers,'sender' => $sender,'message' => $message);
    // Send the POST request with cURL
-   $ch = curl_init('https://smsforall.com/portal/receive_api/api_request?method=sendMessage&mobileno=7483737698&content='.$content.'&loginid=Sdcbpet2&auth_scheme=PLAIN&password=Sajsdc@25&senderid=SDCPUC');
-   curl_setopt($ch, CURLOPT_POST, false);
+   $ch = curl_init('https://api.textlocal.in/send/');
+   curl_setopt($ch, CURLOPT_POST, true);
+   curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
    $response = curl_exec($ch);
    curl_close($ch);
