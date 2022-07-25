ae9d8dc6ef0baed2115a0d3f11d84eaaf0ec50ec
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-25 13:49:42 +0530

    a

--- bangarpet/staff/export/scriptsendsms.php
+++ bangarpet/staff/export/scriptsendsms.php
@@ -75,7 +75,7 @@ SDC COLLEGE BANGARPET-563114');
  
 $numbers = implode(',', $numbers);
  // Prepare data for POST request
-$data = array('apikey' => $apiKey, 'numbers' => $numbers, “sender” => $sender, “message” => $message);
+$data = array('apikey' => $apiKey, 'numbers' => $numbers,'sender' => $sender,'message' => $message);
 // Send the POST request with cURL
 $ch = curl_init('https://api.textlocal.in/send/');
 curl_setopt($ch, CURLOPT_POST, true);
