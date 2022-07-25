bec4e0e49be86ce53f0c16c33fd43e350abe40db
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-25 14:14:45 +0530

    a

--- sms.php
+++ sms.php
@@ -6,17 +6,15 @@
 $message = rawurlencode('Dear pavan kuamr
 sdasds
 SDC sdc ');
-    
-   $numbers = implode(',', $numbers);
     // Prepare data for POST request
-   $data = array('apikey' => $apiKey, 'numbers' => $numbers,'sender' => $sender,'message' => $message);
-   // Send the POST request with cURL
-   $ch = curl_init('https://api.textlocal.in/send/');
-   curl_setopt($ch, CURLOPT_POST, true);
-   curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-   $response = curl_exec($ch);
-   curl_close($ch);
-echo $response;
-echo $message;
+	$data = 'apikey=' . $apiKey . '&numbers=' . $numbers . "&sender=" . $sender . "&message=" . $message;
+ 
+	// Send the GET request with cURL
+	$ch = curl_init('https://api.textlocal.in/send/?' . $data);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$response = curl_exec($ch);
+	curl_close($ch);
+	
+	// Process your response here
+	echo $response;
 ?>
