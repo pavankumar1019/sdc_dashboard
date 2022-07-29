800363178e85ed2b97143941456a9ead1c71453c
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-29 09:20:27 +0530

    a

---
+++ admin/model/includes/footer.php
@@ -0,0 +1,14 @@
+</div>
+
+    <!-- If you prefer jQuery these are the required scripts -->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+    <script src="./dist/js/vendor.js"></script>
+    <script src="./dist/js/adminx.js"></script>
+
+    <!-- If you prefer vanilla JS these are the only required scripts -->
+    <!-- script src="./dist/js/vendor.js"></script>
+    <script src="./dist/js/adminx.vanilla.js"></script-->
+  </body>
+</html>
