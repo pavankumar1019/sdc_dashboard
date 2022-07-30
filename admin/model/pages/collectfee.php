b9e92c9732c67283e61fd8405d0dd02d078ce65b
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-30 09:34:42 +0530

    a

--- admin/model/pages/collectfee.php
+++ admin/model/pages/collectfee.php
@@ -1,51 +1,60 @@
 <div class="adminx-content">
-        <div class="adminx-main-content">
-          <div class="container-fluid">
+    <div class="adminx-main-content">
+        <div class="container-fluid">
             <!-- BreadCrumb -->
             <nav aria-label="breadcrumb" role="navigation">
-              <ol class="breadcrumb adminx-page-breadcrumb">
-                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item"><a href="#">Forms</a></li>
-                <li class="breadcrumb-item active  aria-current="page">Elements</li>
-              </ol>
+                <ol class="breadcrumb adminx-page-breadcrumb">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item"><a href="#">Forms</a></li>
+                    <li class="breadcrumb-item active  aria-current=" page">Elements</li>
+                </ol>
             </nav>
 
             <div class="pb-3">
-              <h1>Elements</h1>
+                <h1>Elements</h1>
             </div>
 
             <div class="row">
-              <div class="col-lg-12">
-               
+                <div class="col-lg-12">
 
 
-                <div class="card mb-grid">
-                  <div class="card-header">
-                    <div class="card-header-title">Column sizing</div>
-                  </div>
-                  <div class="card-body">
-                    <form>
-                      <div class="form-row">
-                        <div class="col-7">
-                          <input type="text" class="form-control" placeholder="City">
+
+                    <div class="card mb-grid">
+                        <div class="card-header">
+                            <div class="card-header-title">Column sizing</div>
                         </div>
-                        <div class="col">
-                          <input type="text" class="form-control" placeholder="State">
-                        </div>
-                        <div class="col">
-                          <input type="text" class="form-control" placeholder="Zip">
-                        </div>
+                        <div class="card-body">
+                            <form>
+                                <div class="form-row">
+                                    <div class="col-7">
+                                    <div class="form-group">
+                        <label class="form-label" for="exampleFormControlSelect1">Example select</label>
+                        <select class="form-control" id="exampleFormControlSelect1">
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                        </select>
                       </div>
-                    </form>
-                  </div>
+                                    </div>
+                                    <div class="col">
+                                        <input type="text" class="form-control" placeholder="State">
+                                    </div>
+                                    <div class="col">
+                                        <input type="text" class="form-control" placeholder="Zip">
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+
+
+
                 </div>
 
-              
-
-               
-              </div>
-
             </div>
-          </div>
         </div>
-      </div>
+    </div>
+</div>
