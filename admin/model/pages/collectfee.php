39178445ad4f78735bf47704c8d70c60a8031883
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-30 12:03:10 +0530

    a

--- admin/model/pages/collectfee.php
+++ admin/model/pages/collectfee.php
@@ -237,7 +237,7 @@
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalCenterTitle">Collect Fee</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -247,7 +247,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-primary">Collect</button>
       </div>
     </div>
   </div>
