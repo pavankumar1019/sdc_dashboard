975f0f8bd728487b3779a709cec0b452997e3bc0
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-07-30 12:02:09 +0530

    a

--- admin/model/pages/collectfee.php
+++ admin/model/pages/collectfee.php
@@ -131,7 +131,7 @@
                                             <span class="badge badge-pill badge-primary">Balance</span>
                                         </td>
                                         <td>
-                                            <button class="btn btn-sm btn-primary">Collect</button>
+                                            <button class="btn btn-sm btn-primary" type="button"  data-toggle="modal" data-target="#exampleModalCenter">Collect</button>
                                             <button class="btn btn-sm btn-danger">Delete</button>
                                         </td>
                                     </tr>
@@ -231,4 +231,24 @@
             </div>
         </div>
     </div>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
 </div>
