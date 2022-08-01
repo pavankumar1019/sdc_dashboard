01135fe859a299e94ba4212c449d151f5457d2fa
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-08-01 09:02:57 +0530

    a

---
+++ admin/model/pages/newadmission.php
@@ -0,0 +1,298 @@
+<div class="adminx-sidebar expand-hover push" id="sidebar">
+        <ul class="sidebar-nav">
+          <li class="sidebar-nav-item">
+            <a href="../index.html" class="sidebar-nav-link">
+              <span class="sidebar-nav-icon">
+                <i data-feather="home"></i>
+              </span>
+              <span class="sidebar-nav-name">
+                Dashboard
+              </span>
+              <span class="sidebar-nav-end">
+
+              </span>
+            </a>
+          </li>
+
+          <li class="sidebar-nav-item">
+            <a href="#" class="sidebar-nav-link">
+              <span class="sidebar-nav-icon">
+                <i data-feather="layout"></i>
+              </span>
+              <span class="sidebar-nav-name">
+                Layout Options
+              </span>
+              <span class="sidebar-nav-end">
+                <span class="badge badge-info">4</span>
+              </span>
+            </a>
+          </li>
+
+          <li class="sidebar-nav-item">
+            <a class="sidebar-nav-link collapsed" data-toggle="collapse" href="#example" aria-expanded="false" aria-controls="example">
+              <span class="sidebar-nav-icon">
+                <i data-feather="pie-chart"></i>
+              </span>
+              <span class="sidebar-nav-name">
+                Charts
+              </span>
+              <span class="sidebar-nav-end">
+                <i data-feather="chevron-right" class="nav-collapse-icon"></i>
+              </span>
+            </a>
+
+            <ul class="sidebar-sub-nav collapse" id="example">
+              <li class="sidebar-nav-item">
+                <a href="./charts_chartjs.html" class="sidebar-nav-link">
+                  <span class="sidebar-nav-abbr">
+                    Ch
+                  </span>
+                  <span class="sidebar-nav-name">
+                    ChartJS
+                  </span>
+                </a>
+              </li>
+
+              <li class="sidebar-nav-item">
+                <a href="./charts_morris.html" class="sidebar-nav-link">
+                  <span class="sidebar-nav-abbr">
+                    Mo
+                  </span>
+                  <span class="sidebar-nav-name">
+                    Morris
+                  </span>
+                </a>
+              </li>
+            </ul>
+          </li>
+
+          <li class="sidebar-nav-item">
+            <a class="sidebar-nav-link collapsed" data-toggle="collapse" href="#navTables" aria-expanded="false" aria-controls="navTables">
+              <span class="sidebar-nav-icon">
+                <i data-feather="align-justify"></i>
+              </span>
+              <span class="sidebar-nav-name">
+                Tables
+              </span>
+              <span class="sidebar-nav-end">
+                <i data-feather="chevron-right" class="nav-collapse-icon"></i>
+              </span>
+            </a>
+
+            <ul class="sidebar-sub-nav collapse" id="navTables">
+              <li class="sidebar-nav-item">
+                <a href="./tables.html" class="sidebar-nav-link">
+                  <span class="sidebar-nav-abbr">
+                    Re
+                  </span>
+                  <span class="sidebar-nav-name">
+                    Regular Tables
+                  </span>
+                </a>
+              </li>
+
+              <li class="sidebar-nav-item">
+                <a href="./tables_data.html" class="sidebar-nav-link">
+                  <span class="sidebar-nav-abbr">
+                    Da
+                  </span>
+                  <span class="sidebar-nav-name">
+                    Data Tables
+                  </span>
+                </a>
+              </li>
+            </ul>
+          </li>
+
+          <li class="sidebar-nav-item">
+            <a class="sidebar-nav-link" data-toggle="collapse" href="#navForms" aria-expanded="false" aria-controls="navForms">
+              <span class="sidebar-nav-icon">
+                <i data-feather="edit"></i>
+              </span>
+              <span class="sidebar-nav-name">
+                Forms
+              </span>
+              <span class="sidebar-nav-end">
+                <i data-feather="chevron-right" class="nav-collapse-icon"></i>
+              </span>
+            </a>
+
+            <ul class="sidebar-sub-nav collapse show" id="navForms">
+              <li class="sidebar-nav-item">
+                <a href="./form_elements.html" class="sidebar-nav-link active">
+                  <span class="sidebar-nav-abbr">
+                    El
+                  </span>
+                  <span class="sidebar-nav-name">
+                    Elements
+                  </span>
+                </a>
+              </li>
+
+              <li class="sidebar-nav-item">
+                <a href="./form_advanced.html" class="sidebar-nav-link">
+                  <span class="sidebar-nav-abbr">
+                    Ad
+                  </span>
+                  <span class="sidebar-nav-name">
+                    Advanced Elements
+                  </span>
+                </a>
+              </li>
+            </ul>
+          </li>
+
+          <li class="sidebar-nav-item">
+            <a class="sidebar-nav-link collapsed" data-toggle="collapse" href="#navUI" aria-expanded="false" aria-controls="navUI">
+              <span class="sidebar-nav-icon">
+                <i data-feather="grid"></i>
+              </span>
+              <span class="sidebar-nav-name">
+                UI Elements
+              </span>
+              <span class="sidebar-nav-end">
+                <i data-feather="chevron-right" class="nav-collapse-icon"></i>
+              </span>
+            </a>
+
+            <ul class="sidebar-sub-nav collapse" id="navUI">
+              <li class="sidebar-nav-item">
+                <a href="./icons.html" class="sidebar-nav-link">
+                  <span class="sidebar-nav-abbr">
+                    Ic
+                  </span>
+                  <span class="sidebar-nav-name">
+                    Icons
+                  </span>
+                </a>
+              </li>
+
+              <li class="sidebar-nav-item">
+                <a href="./buttons.html" class="sidebar-nav-link">
+                  <span class="sidebar-nav-abbr">
+                    Bu
+                  </span>
+                  <span class="sidebar-nav-name">
+                    Buttons
+                  </span>
+                </a>
+              </li>
+
+              <li class="sidebar-nav-item">
+                <a href="./notifications.html" class="sidebar-nav-link">
+                  <span class="sidebar-nav-abbr">
+                    No
+                  </span>
+                  <span class="sidebar-nav-name">
+                    Notifications
+                  </span>
+                </a>
+              </li>
+
+              <li class="sidebar-nav-item">
+                <a href="./progress.html" class="sidebar-nav-link">
+                  <span class="sidebar-nav-abbr">
+                    Pr
+                  </span>
+                  <span class="sidebar-nav-name">
+                    Progress Bars
+                  </span>
+                </a>
+              </li>
+
+              <li class="sidebar-nav-item">
+                <a href="./tabs.html" class="sidebar-nav-link">
+                  <span class="sidebar-nav-abbr">
+                    Ta
+                  </span>
+                  <span class="sidebar-nav-name">
+                    Tabs
+                  </span>
+                </a>
+              </li>
+            </ul>
+          </li>
+
+          <li class="sidebar-nav-item">
+            <a class="sidebar-nav-link collapsed" data-toggle="collapse" href="#navExtra" aria-expanded="false" aria-controls="navExtra">
+              <span class="sidebar-nav-icon">
+                <i data-feather="layers"></i>
+              </span>
+              <span class="sidebar-nav-name">
+                Other Layouts
+              </span>
+              <span class="sidebar-nav-end">
+                <i data-feather="chevron-right" class="nav-collapse-icon"></i>
+              </span>
+            </a>
+
+            <ul class="sidebar-sub-nav collapse" id="navExtra">
+              <li class="sidebar-nav-item">
+                <a href="./login.html" class="sidebar-nav-link">
+                  <span class="sidebar-nav-abbr">
+                    Lo
+                  </span>
+                  <span class="sidebar-nav-name">
+                    Login
+                  </span>
+                </a>
+              </li>
+
+              <li class="sidebar-nav-item">
+                <a href="./signup.html" class="sidebar-nav-link">
+                  <span class="sidebar-nav-abbr">
+                    Si
+                  </span>
+                  <span class="sidebar-nav-name">
+                    Sign Up
+                  </span>
+                </a>
+              </li>
+
+              <li class="sidebar-nav-item">
+                <a href="./404.html" class="sidebar-nav-link">
+                  <span class="sidebar-nav-abbr">
+                    Nf
+                  </span>
+                  <span class="sidebar-nav-name">
+                    404 Error
+                  </span>
+                </a>
+              </li>
+
+              <li class="sidebar-nav-item">
+                <a href="./500.html" class="sidebar-nav-link">
+                  <span class="sidebar-nav-abbr">
+                    Is
+                  </span>
+                  <span class="sidebar-nav-name">
+                    500 Error
+                  </span>
+                </a>
+              </li>
+
+              <li class="sidebar-nav-item">
+                <a href="./timeline.html" class="sidebar-nav-link">
+                  <span class="sidebar-nav-abbr">
+                    Ti
+                  </span>
+                  <span class="sidebar-nav-name">
+                    Timeline
+                  </span>
+                </a>
+              </li>
+
+              <li class="sidebar-nav-item">
+                <a href="./invoice.html" class="sidebar-nav-link">
+                  <span class="sidebar-nav-abbr">
+                    In
+                  </span>
+                  <span class="sidebar-nav-name">
+                    Invoice
+                  </span>
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
