0463e070fcf9c61beed7fa96a870dca68b7b2cea
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-08-01 09:07:50 +0530

    a

--- admin/model/pages/newadmission.php
+++ admin/model/pages/newadmission.php
@@ -16,117 +16,29 @@
 
             <div class="row">
                 <div class="col-lg-6">
-                    <div class="card mb-grid">
-                        <div class="card-header d-flex justify-content-between align-items-center">
-                            <div class="card-header-title">Basic Form Example</div>
-
-                            <nav class="card-header-actions">
-                                <a class="card-header-action" data-toggle="collapse" href="#card1" aria-expanded="false"
-                                    aria-controls="card1">
-                                    <i data-feather="minus-circle"></i>
-                                </a>
-
-                                <div class="dropdown">
-                                    <a class="card-header-action" href="#" role="button" id="card1Settings"
-                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <i data-feather="settings"></i>
-                                    </a>
-
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="card1Settings">
-                                        <a class="dropdown-item" href="#">Action</a>
-                                        <a class="dropdown-item" href="#">Another action</a>
-                                        <a class="dropdown-item" href="#">Something else here</a>
-                                    </div>
-                                </div>
-
-                                <a href="#" class="card-header-action">
-                                    <i data-feather="x-circle"></i>
-                                </a>
-                            </nav>
+                <div class="card mb-grid">
+                  <div class="card-header">
+                    <div class="card-header-title">Column sizing</div>
+                  </div>
+                  <div class="card-body">
+                    <form>
+                      <div class="form-row">
+                        <div class="col-7">
+                          <input type="text" class="form-control" placeholder="City">
                         </div>
-
-                    </div>
-
-
-
-
-
+                        <div class="col">
+                          <input type="text" class="form-control" placeholder="State">
+                        </div>
+                        <div class="col">
+                          <input type="text" class="form-control" placeholder="Zip">
+                        </div>
+                      </div>
+                    </form>
+                  </div>
                 </div>
 
-                <div class="col-lg-6">
-                    <div class="card mb-grid">
-                        <div class="card-header">
-                            <div class="card-header-title">Horizontal form</div>
-                        </div>
-                        <div class="card-body">
-                            <form>
-                                <div class="form-group row">
-                                    <label for="inputEmail3" class="col-sm-2 col-form-label form-label">Email</label>
-                                    <div class="col-sm-10">
-                                        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label for="inputPassword3"
-                                        class="col-sm-2 col-form-label form-label">Password</label>
-                                    <div class="col-sm-10">
-                                        <input type="password" class="form-control" id="inputPassword3"
-                                            placeholder="Password">
-                                    </div>
-                                </div>
-                                <fieldset class="form-group">
-                                    <div class="row">
-                                        <legend class="col-form-legend col-sm-2 form-label">Radios</legend>
-                                        <div class="col-sm-10">
-                                            <div class="form-check">
-                                                <label class="form-check-label">
-                                                    <input class="form-check-input" type="radio" name="gridRadios"
-                                                        id="gridRadios1" value="option1" checked>
-                                                    Option one is this and that&mdash;be sure to include why it's great
-                                                </label>
-                                            </div>
-                                            <div class="form-check">
-                                                <label class="form-check-label">
-                                                    <input class="form-check-input" type="radio" name="gridRadios"
-                                                        id="gridRadios2" value="option2">
-                                                    Option two can be something else and selecting it will deselect
-                                                    option one
-                                                </label>
-                                            </div>
-                                            <div class="form-check disabled">
-                                                <label class="form-check-label">
-                                                    <input class="form-check-input" type="radio" name="gridRadios"
-                                                        id="gridRadios3" value="option3" disabled>
-                                                    Option three is disabled
-                                                </label>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </fieldset>
-                                <div class="form-group row">
-                                    <div class="col-sm-2 form-label pt-1">Checkbox</div>
-                                    <div class="col-sm-10">
-                                        <div class="form-check">
-                                            <label class="form-check-label">
-                                                <input class="form-check-input" type="checkbox"> Check me out
-                                            </label>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <div class="col-sm-10">
-                                        <button type="submit" class="btn btn-primary">Sign in</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-
-
-
-
                 </div>
-            </div>
+                        </div>
         </div>
     </div>
 </div>
