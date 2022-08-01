150ac97576903b3b137f57089bf9d2c00a20195d
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-08-01 09:05:31 +0530

    a

--- admin/model/pages/newadmission.php
+++ admin/model/pages/newadmission.php
@@ -1,298 +1,132 @@
-<div class="adminx-sidebar expand-hover push" id="sidebar">
-        <ul class="sidebar-nav">
-          <li class="sidebar-nav-item">
-            <a href="../index.html" class="sidebar-nav-link">
-              <span class="sidebar-nav-icon">
-                <i data-feather="home"></i>
-              </span>
-              <span class="sidebar-nav-name">
-                Dashboard
-              </span>
-              <span class="sidebar-nav-end">
+<div class="adminx-content">
+    <div class="adminx-main-content">
+        <div class="container-fluid">
+            <!-- BreadCrumb -->
+            <nav aria-label="breadcrumb" role="navigation">
+                <ol class="breadcrumb adminx-page-breadcrumb">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item"><a href="#">Forms</a></li>
+                    <li class="breadcrumb-item active  aria-current=" page">Elements</li>
+                </ol>
+            </nav>
 
-              </span>
-            </a>
-          </li>
+            <div class="pb-3">
+                <h1>Elements</h1>
+            </div>
 
-          <li class="sidebar-nav-item">
-            <a href="#" class="sidebar-nav-link">
-              <span class="sidebar-nav-icon">
-                <i data-feather="layout"></i>
-              </span>
-              <span class="sidebar-nav-name">
-                Layout Options
-              </span>
-              <span class="sidebar-nav-end">
-                <span class="badge badge-info">4</span>
-              </span>
-            </a>
-          </li>
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="card mb-grid">
+                        <div class="card-header d-flex justify-content-between align-items-center">
+                            <div class="card-header-title">Basic Form Example</div>
 
-          <li class="sidebar-nav-item">
-            <a class="sidebar-nav-link collapsed" data-toggle="collapse" href="#example" aria-expanded="false" aria-controls="example">
-              <span class="sidebar-nav-icon">
-                <i data-feather="pie-chart"></i>
-              </span>
-              <span class="sidebar-nav-name">
-                Charts
-              </span>
-              <span class="sidebar-nav-end">
-                <i data-feather="chevron-right" class="nav-collapse-icon"></i>
-              </span>
-            </a>
+                            <nav class="card-header-actions">
+                                <a class="card-header-action" data-toggle="collapse" href="#card1" aria-expanded="false"
+                                    aria-controls="card1">
+                                    <i data-feather="minus-circle"></i>
+                                </a>
 
-            <ul class="sidebar-sub-nav collapse" id="example">
-              <li class="sidebar-nav-item">
-                <a href="./charts_chartjs.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Ch
-                  </span>
-                  <span class="sidebar-nav-name">
-                    ChartJS
-                  </span>
-                </a>
-              </li>
+                                <div class="dropdown">
+                                    <a class="card-header-action" href="#" role="button" id="card1Settings"
+                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i data-feather="settings"></i>
+                                    </a>
 
-              <li class="sidebar-nav-item">
-                <a href="./charts_morris.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Mo
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Morris
-                  </span>
-                </a>
-              </li>
-            </ul>
-          </li>
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="card1Settings">
+                                        <a class="dropdown-item" href="#">Action</a>
+                                        <a class="dropdown-item" href="#">Another action</a>
+                                        <a class="dropdown-item" href="#">Something else here</a>
+                                    </div>
+                                </div>
 
-          <li class="sidebar-nav-item">
-            <a class="sidebar-nav-link collapsed" data-toggle="collapse" href="#navTables" aria-expanded="false" aria-controls="navTables">
-              <span class="sidebar-nav-icon">
-                <i data-feather="align-justify"></i>
-              </span>
-              <span class="sidebar-nav-name">
-                Tables
-              </span>
-              <span class="sidebar-nav-end">
-                <i data-feather="chevron-right" class="nav-collapse-icon"></i>
-              </span>
-            </a>
+                                <a href="#" class="card-header-action">
+                                    <i data-feather="x-circle"></i>
+                                </a>
+                            </nav>
+                        </div>
 
-            <ul class="sidebar-sub-nav collapse" id="navTables">
-              <li class="sidebar-nav-item">
-                <a href="./tables.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Re
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Regular Tables
-                  </span>
-                </a>
-              </li>
+                    </div>
 
-              <li class="sidebar-nav-item">
-                <a href="./tables_data.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Da
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Data Tables
-                  </span>
-                </a>
-              </li>
-            </ul>
-          </li>
 
-          <li class="sidebar-nav-item">
-            <a class="sidebar-nav-link" data-toggle="collapse" href="#navForms" aria-expanded="false" aria-controls="navForms">
-              <span class="sidebar-nav-icon">
-                <i data-feather="edit"></i>
-              </span>
-              <span class="sidebar-nav-name">
-                Forms
-              </span>
-              <span class="sidebar-nav-end">
-                <i data-feather="chevron-right" class="nav-collapse-icon"></i>
-              </span>
-            </a>
 
-            <ul class="sidebar-sub-nav collapse show" id="navForms">
-              <li class="sidebar-nav-item">
-                <a href="./form_elements.html" class="sidebar-nav-link active">
-                  <span class="sidebar-nav-abbr">
-                    El
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Elements
-                  </span>
-                </a>
-              </li>
 
-              <li class="sidebar-nav-item">
-                <a href="./form_advanced.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Ad
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Advanced Elements
-                  </span>
-                </a>
-              </li>
-            </ul>
-          </li>
 
-          <li class="sidebar-nav-item">
-            <a class="sidebar-nav-link collapsed" data-toggle="collapse" href="#navUI" aria-expanded="false" aria-controls="navUI">
-              <span class="sidebar-nav-icon">
-                <i data-feather="grid"></i>
-              </span>
-              <span class="sidebar-nav-name">
-                UI Elements
-              </span>
-              <span class="sidebar-nav-end">
-                <i data-feather="chevron-right" class="nav-collapse-icon"></i>
-              </span>
-            </a>
+                </div>
 
-            <ul class="sidebar-sub-nav collapse" id="navUI">
-              <li class="sidebar-nav-item">
-                <a href="./icons.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Ic
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Icons
-                  </span>
-                </a>
-              </li>
+                <div class="col-lg-6">
+                    <div class="card mb-grid">
+                        <div class="card-header">
+                            <div class="card-header-title">Horizontal form</div>
+                        </div>
+                        <div class="card-body">
+                            <form>
+                                <div class="form-group row">
+                                    <label for="inputEmail3" class="col-sm-2 col-form-label form-label">Email</label>
+                                    <div class="col-sm-10">
+                                        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="inputPassword3"
+                                        class="col-sm-2 col-form-label form-label">Password</label>
+                                    <div class="col-sm-10">
+                                        <input type="password" class="form-control" id="inputPassword3"
+                                            placeholder="Password">
+                                    </div>
+                                </div>
+                                <fieldset class="form-group">
+                                    <div class="row">
+                                        <legend class="col-form-legend col-sm-2 form-label">Radios</legend>
+                                        <div class="col-sm-10">
+                                            <div class="form-check">
+                                                <label class="form-check-label">
+                                                    <input class="form-check-input" type="radio" name="gridRadios"
+                                                        id="gridRadios1" value="option1" checked>
+                                                    Option one is this and that&mdash;be sure to include why it's great
+                                                </label>
+                                            </div>
+                                            <div class="form-check">
+                                                <label class="form-check-label">
+                                                    <input class="form-check-input" type="radio" name="gridRadios"
+                                                        id="gridRadios2" value="option2">
+                                                    Option two can be something else and selecting it will deselect
+                                                    option one
+                                                </label>
+                                            </div>
+                                            <div class="form-check disabled">
+                                                <label class="form-check-label">
+                                                    <input class="form-check-input" type="radio" name="gridRadios"
+                                                        id="gridRadios3" value="option3" disabled>
+                                                    Option three is disabled
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </fieldset>
+                                <div class="form-group row">
+                                    <div class="col-sm-2 form-label pt-1">Checkbox</div>
+                                    <div class="col-sm-10">
+                                        <div class="form-check">
+                                            <label class="form-check-label">
+                                                <input class="form-check-input" type="checkbox"> Check me out
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-10">
+                                        <button type="submit" class="btn btn-primary">Sign in</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
 
-              <li class="sidebar-nav-item">
-                <a href="./buttons.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Bu
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Buttons
-                  </span>
-                </a>
-              </li>
 
-              <li class="sidebar-nav-item">
-                <a href="./notifications.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    No
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Notifications
-                  </span>
-                </a>
-              </li>
 
-              <li class="sidebar-nav-item">
-                <a href="./progress.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Pr
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Progress Bars
-                  </span>
-                </a>
-              </li>
 
-              <li class="sidebar-nav-item">
-                <a href="./tabs.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Ta
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Tabs
-                  </span>
-                </a>
-              </li>
-            </ul>
-          </li>
-
-          <li class="sidebar-nav-item">
-            <a class="sidebar-nav-link collapsed" data-toggle="collapse" href="#navExtra" aria-expanded="false" aria-controls="navExtra">
-              <span class="sidebar-nav-icon">
-                <i data-feather="layers"></i>
-              </span>
-              <span class="sidebar-nav-name">
-                Other Layouts
-              </span>
-              <span class="sidebar-nav-end">
-                <i data-feather="chevron-right" class="nav-collapse-icon"></i>
-              </span>
-            </a>
-
-            <ul class="sidebar-sub-nav collapse" id="navExtra">
-              <li class="sidebar-nav-item">
-                <a href="./login.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Lo
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Login
-                  </span>
-                </a>
-              </li>
-
-              <li class="sidebar-nav-item">
-                <a href="./signup.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Si
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Sign Up
-                  </span>
-                </a>
-              </li>
-
-              <li class="sidebar-nav-item">
-                <a href="./404.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Nf
-                  </span>
-                  <span class="sidebar-nav-name">
-                    404 Error
-                  </span>
-                </a>
-              </li>
-
-              <li class="sidebar-nav-item">
-                <a href="./500.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Is
-                  </span>
-                  <span class="sidebar-nav-name">
-                    500 Error
-                  </span>
-                </a>
-              </li>
-
-              <li class="sidebar-nav-item">
-                <a href="./timeline.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    Ti
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Timeline
-                  </span>
-                </a>
-              </li>
-
-              <li class="sidebar-nav-item">
-                <a href="./invoice.html" class="sidebar-nav-link">
-                  <span class="sidebar-nav-abbr">
-                    In
-                  </span>
-                  <span class="sidebar-nav-name">
-                    Invoice
-                  </span>
-                </a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
