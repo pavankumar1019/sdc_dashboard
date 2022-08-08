8d8cb47c20bebd3383ad9ca870a0a9df6e94ccee
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-08-08 14:19:50 +0530

    a

--- rastradevobhava/index.php
+++ rastradevobhava/index.php
@@ -49,7 +49,10 @@
 
     </select>
   </div>
- 
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Contact No.</label>
+    <input type="number" class="form-control" id="exampleFormControlInput1" >
+  </div>
   <div class="form-group">
     <label for="exampleFormControlTextarea1">Example textarea</label>
     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
