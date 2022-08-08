6cfd8bd75680a4b6dfb4f05c654d0406bea00768
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-08-08 16:59:43 +0530

    a

--- rastradevobhava/save.php
+++ rastradevobhava/save.php
@@ -17,6 +17,25 @@ if(!$_POST['name']=""){
 	$sql = "INSERT INTO `events`( `name`, `parents`, `dob`, `address`, `class`, `branch`, `contact`, `event`, `email`) 
 	VALUES ('".$_POST['name_std']."','".$_POST['parent']."','".$_POST['dob']."','".$_POST['address']."','".$_POST['class']."','".$_POST['branch']."','".$_POST['contact']."','".$_POST['event']."','".$_POST['email']."')";
 	if (mysqli_query($conn, $sql)) {
+        
+$apiKey = urlencode('NDM1OTMzNTA0MjUyMzk2MzVhNGUzMDQ4NzY3NTM5Njc=');
+// Message details
+$numbers = array($_POST['contact']);
+$sender = urlencode('SDCPUC');
+$message = rawurlencode('Dear Student 
+ Thank you! Registration Successfull for Rastradevobhava inter branch competation
+SDC COLLEGE BANGARPET-563114');
+ 
+$numbers = implode(',', $numbers);
+ // Prepare data for POST request
+$data = array('apikey' => $apiKey, 'numbers' => $numbers,'sender' => $sender,'message' => $message);
+// Send the POST request with cURL
+$ch = curl_init('https://api.textlocal.in/send/');
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
 		echo "Done you have registered successfully !";
 	} 
 	else {
