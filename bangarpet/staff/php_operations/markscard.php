31aafe7cfa57383f90a774c7dcd67f33073ca118
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-08-08 11:20:45 +0530

    a

--- bangarpet/staff/php_operations/markscard.php
+++ bangarpet/staff/php_operations/markscard.php
@@ -317,7 +317,7 @@ if($_POST['type']=="edit"){
   $query = 
   'SELECT '.$tbl_admission.'.StudentName,  '.$tbl_admission.'.father_name, '.$tbl_admission.'.mobile_no, '.$tbl_admission.'.RollNo, '.$class_test_marks.'.l1, '.$class_test_marks.'.l2,  '.$class_test_marks.'.s1, '.$class_test_marks.'.s2,  '.$class_test_marks.'.s3,  '.$class_test_marks.'.s4,  '.$class_test_marks.'.total
     FROM '.$tbl_admission.'
-    LEFT JOIN '.$class_test_marks.' ON '.$tbl_admission.'.RollNo = '.$class_test_marks.'.roll  WHERE '.$tbl_admission.'.Class='.$_SESSION['class_id'].' AND '.$class_test_marks.'test_id="'. $_SESSION['test_name'].'" AND '.$tbl_admission.'.RollNo="'.$_POST['id'].'"
+    LEFT JOIN '.$class_test_marks.' ON '.$tbl_admission.'.RollNo = '.$class_test_marks.'.roll AND ".$class_test_marks.".test_id="'. $_SESSION['test_name'].'"  WHERE '.$tbl_admission.'.Class='.$_SESSION['class_id'].' AND '.$tbl_admission.'.RollNo="'.$_POST['id'].'"
     '
   ;
   $statement = $connect->prepare($query);
