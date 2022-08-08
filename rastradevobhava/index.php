3f5d7d207ea5346263ada2058aa771cfa6ed3076
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-08-08 14:09:03 +0530

    a

--- rastradevobhava/index.php
+++ rastradevobhava/index.php
@@ -11,6 +11,7 @@
 </head>
 <body>
 <section class="m-5">
+    <h2>Rastradevobhava</h2>
 <form>
   <div class="form-group">
     <label for="exampleFormControlInput1">Email address</label>
@@ -26,16 +27,7 @@
       <option>5</option>
     </select>
   </div>
-  <div class="form-group">
-    <label for="exampleFormControlSelect2">Example multiple select</label>
-    <select multiple class="form-control" id="exampleFormControlSelect2">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-    </select>
-  </div>
+ 
   <div class="form-group">
     <label for="exampleFormControlTextarea1">Example textarea</label>
     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
