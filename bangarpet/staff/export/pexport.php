ae0d43c5eab0e15d75af9847b02525af441bf29f
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-08-10 10:37:39 +0530

    a

--- bangarpet/staff/export/pexport.php
+++ bangarpet/staff/export/pexport.php
@@ -195,7 +195,7 @@ $output .= '</table>';
 $query2 = "SELECT   ".$tbl_admission.".StudentName, ".$class_test_marks.".total
   FROM ".$tbl_admission."
   LEFT JOIN ".$class_test_marks." ON ".$tbl_admission.".RollNo = ".$class_test_marks.".roll AND ".$class_test_marks.".test_id=". $_SESSION['test_name']."    WHERE ".$tbl_admission.".Class=".$_GET['class_id']."  
-  ORDER BY `".$class_test_marks."`.`total` DESC LIMIT 5";
+  ORDER BY `".$class_test_marks."`.`total` DESC LIMIT 20";
 $result2 = mysqli_query($conn, $query2);
 
 if(mysqli_num_rows($result2) > 0)
