1cfecbdbb604ef13e48e311d9743418f182b4549
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-08-16 09:49:06 +0530

    a

--- bangarpet/staff/php_operations/approvestaff.php
+++ bangarpet/staff/php_operations/approvestaff.php
@@ -81,9 +81,7 @@ if($_POST['type']=="loaddata"){
 
         $output .= '
         <tr class="unread">
-                                                  <!--  <td>
-                                                 <img src="'.$row['photo'].'" width="50">
-                                                    </td>-->
+                                           
                                                     <td>
                                                         <h3 class="mb-1">'.$row['name'].'</h3>
                                                        
