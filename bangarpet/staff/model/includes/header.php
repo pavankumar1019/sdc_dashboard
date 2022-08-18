e62cc6314eb3b4a34d3430e782f84917b2b98589
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-08-18 08:46:40 +0530

    a

--- bangarpet/staff/model/includes/header.php
+++ bangarpet/staff/model/includes/header.php
@@ -95,7 +95,7 @@
                 ?>
                 <?php
                 if($_SESSION['role_staff']=="P"){
-                    echo '<li data-username="Sample Page" class="nav-item"><a href="./dashboard.php?page=smsmarks" class="nav-link"><span class="pcoded-micon"><i class="feather icon-mail"></i></span><span class="pcoded-mtext">SMS MARKS </span></a></li>
+                    echo '<li data-username="Sample Page" class="nav-item"><a href="./dashboard.php?page=smsmarks" class="nav-link"><span class="pcoded-micon"><i class="feather icon-mail"></i></span><span class="pcoded-mtext">SMS MARKS '.$_SESSION['id'].' </span></a></li>
                     ';
                 }
                 ?>
