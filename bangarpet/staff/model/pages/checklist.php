d3f451613685c370eb6289696b793bb7a134425e
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-08-18 09:21:43 +0530

    a

--- bangarpet/staff/model/pages/checklist.php
+++ bangarpet/staff/model/pages/checklist.php
@@ -1,5 +1,7 @@
 <?php
+include('./db_bpet_sdc/db.php');
                 if($_SESSION['id']==13 || $_SESSION['id']==21){
+
                  ?>
 <div class="pcoded-main-container">
     <div class="pcoded-wrapper">
@@ -10,6 +12,15 @@
   <div class="form-row">
   <select class="form-control form-control-lg">
   <option>Select Class</option>
+  <?php
+                        
+                        $sql="SELECT * FROM class";
+                     $result=$conn->query($sql);
+                     foreach($result as $row){
+                         echo '<option value="'.$row['id'].'">'.$row['section'].' - '.$row['name'].'</option>';
+                     }
+                     
+                        ?>
 </select>
    
     
