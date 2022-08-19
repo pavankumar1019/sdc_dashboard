1734bdcd4b36f4786d021b239592e81743dcd7c2
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-08-19 10:39:17 +0530

    a

--- bangarpet/staff/model/pages/examcoordinate.php
+++ bangarpet/staff/model/pages/examcoordinate.php
@@ -18,23 +18,13 @@ include('./db_bpet_sdc/db.php');
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+    <tr style="border: 1px solid black;">
+      <th scope="row" style="border: 1px solid black;">1</th>
+      <td style="border: 1px solid black;">Mark</td>
+      <td style="border: 1px solid black;">Otto</td>
+      <td style="border: 1px solid black;">@mdo</td>
     </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
+   
   </tbody>
 </table>
 
