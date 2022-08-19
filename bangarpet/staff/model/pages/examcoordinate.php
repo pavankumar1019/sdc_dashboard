ba1a8f2e0b8758f4a75a0f30c44484e7b2e77ef7
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-08-19 10:53:03 +0530

    a

--- bangarpet/staff/model/pages/examcoordinate.php
+++ bangarpet/staff/model/pages/examcoordinate.php
@@ -8,23 +8,7 @@ include('./db_bpet_sdc/db.php');
         <div class="pcoded-content">
             <div class="pcoded-inner-content">
 
-            <table class="table table-bordered" style="border: 1px solid black;">
-  <thead>
-    <tr>
-      <th scope="col" style="border: 1px solid black;" colspan="5"><h4>ADD Exam</h2></th>
-     
-    </tr>
-  </thead>
-  <tbody>
-    <tr style="border: 1px solid black;">
-      <th scope="row" style="border: 1px solid black;"><input type="text" name="" id=""></th>
-      <td style="border: 1px solid black;"><input type="text" name="" id=""></td>
-      <td style="border: 1px solid black;"><button class="btn btn-primary">ADD</button></td>
-    </tr>
-   
-  </tbody>
-</table>
-
+       
 
             </div>
         </div>
