cdb48b9fbc1da4bdb405d7b2ea16408bd001e799
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-08-19 11:01:03 +0530

    a

--- bangarpet/staff/model/pages/examcoordinate.php
+++ bangarpet/staff/model/pages/examcoordinate.php
@@ -8,13 +8,14 @@ include('./db_bpet_sdc/db.php');
         <div class="pcoded-content">
             <div class="pcoded-inner-content">
 
-            <div class="row">
-  <div class="col-lg-6 col-sm-3" style="border:1px solid black">.col-6 .col-sm-3</div>
-  <div class="col-lg-6 col-sm-3" style="border:1px solid black">.col-6 .col-sm-3</div>
-  <div class="col-lg-6 col-sm-3" style="border:1px solid black">.col-6 .col-sm-3</div>
-  <div class="col-lg-6 col-sm-3" style="border:1px solid black">.col-6 .col-sm-3</div>
-</div>
-
+                <div class="row">
+                    <div class="col-lg-6 col-sm-3" style="border:1px solid black">.col-6 .col-sm-3</div>
+                    <div class="col-lg-6 col-sm-3" style="border:1px solid black">.col-6 .col-sm-3</div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6 col-sm-3" style="border:1px solid black">.col-6 .col-sm-3</div>
+                    <div class="col-lg-6 col-sm-3" style="border:1px solid black">.col-6 .col-sm-3</div>
+                </div>
             </div>
         </div>
     </div>
