f598f012ccb202b0f4aff1c00a23e5152ea7310c
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-08-19 11:13:56 +0530

    a

--- bangarpet/staff/model/pages/examcoordinate.php
+++ bangarpet/staff/model/pages/examcoordinate.php
@@ -12,27 +12,32 @@ include('./db_bpet_sdc/db.php');
                     <div class="form-row" >
                         <div class="col-6">
                             <div class="form-group">
-                                <label for="exampleFormControlInput1">Email address</label>
+                                <label for="exampleFormControlInput1">Test Name</label>
                                 <input type="email" class="form-control" id="exampleFormControlInput1"
                                     placeholder="name@example.com">
                             </div>
                         </div>
                         <div class="col-6">
                             <div class="form-group">
-                                <label for="exampleFormControlInput1">Email address</label>
+                                <label for="exampleFormControlInput1">Min Marks</label>
+                                <input type="email" class="form-control" id="exampleFormControlInput1"
+                                    placeholder="name@example.com">
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="form-group">
+                                <label for="exampleFormControlInput1">Max Marks</label>
                                 <input type="email" class="form-control" id="exampleFormControlInput1"
                                     placeholder="name@example.com">
                             </div>
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="exampleFormControlSelect1">Example select</label>
+                        <label for="exampleFormControlSelect1">Select Class</label>
                         <select class="form-control" id="exampleFormControlSelect1">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
+                            <option value="1">1st PUC</option>
+                            <option value="2">2nd PUC</option>
+                        
                         </select>
                     </div>
                 </form>
