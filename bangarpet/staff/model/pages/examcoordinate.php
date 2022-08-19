4e673f3657dc0894820e1bec00a7be135659228d
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-08-19 08:39:45 +0530

    a

---
+++ bangarpet/staff/model/pages/examcoordinate.php
@@ -0,0 +1,48 @@
+<?php
+include('./db_bpet_sdc/db.php');
+                if($_SESSION['id']==32 || $_SESSION['id']==21){
+
+                 ?>
+<div class="pcoded-main-container">
+    <div class="pcoded-wrapper">
+        <div class="pcoded-content">
+            <div class="pcoded-inner-content">
+
+            <table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+                }
+                ?>
