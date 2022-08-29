4c7b308e7842a5d03eede1c3510ed73f3ae24e70
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-08-29 17:36:17 +0530

    a

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+
+$method = 'sendMessage';
+	$stdnumber='PAVAN KUMAR S';
+    // Message details
+$content =  rawurlencode('Dear '.$stdnumber.' was absent on todays class at SDC College Bangarpet/KGF.- SDC');
+          
+    
+    
+    // Prepare data for POST request
+    
+    // Send the POST request with cURL
+    $ch = curl_init('https://smsforall.com/portal/receive_api/api_request?method=sendMessage&mobileno='.$stdnumber['mobile_no'].'&content='.$content.'&loginid=Sdcbpet2&auth_scheme=PLAIN&password=Sajsdc@25&senderid=SDCPUC');
+    curl_setopt($ch, CURLOPT_POST, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    echo $stdnumber."&nbsp;-&nbsp; Message Sent";
+?>
