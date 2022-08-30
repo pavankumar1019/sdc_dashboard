c1d42ca2b70005e4b4d0411b34bc0a96e164a7f3
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-08-30 12:04:25 +0530

    a

--- bangarpet/staff/assets/js/markscard.js
+++ bangarpet/staff/assets/js/markscard.js
@@ -36,8 +36,8 @@ function countdata()
 }
 
 
-
-    load_data(1, "", $('#testname_p').val());
+var testvalname=$('#testname_p').val();
+    load_data(1, "", testvalname);
 
     function load_data(page, query = '', testvalue)
     {
