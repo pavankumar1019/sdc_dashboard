ccd1b9a5b0c9d81a0901387c729b8a48d2ea1ee6
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-08-30 12:59:15 +0530

    a

--- bangarpet/staff/model/includes/header.php
+++ bangarpet/staff/model/includes/header.php
@@ -82,7 +82,7 @@
                 ?>
                 <?php
                 if($_SESSION['role_staff']=="ct"){
-                    echo '<li data-username="Sample Page" class="nav-item"><a id="consolidate" class="nav-link"><span class="pcoded-micon"><i class="feather icon-file-text"></i></span><span class="pcoded-mtext">Download Consolidate</span></a></li>';
+                    echo '<li data-username="Sample Page" class="nav-item"><a href="./dashboard.php?page=exportConsolidate"  class="nav-link"><span class="pcoded-micon"><i class="feather icon-file-text"></i></span><span class="pcoded-mtext">Download Consolidate</span></a></li>';
                 }
                 ?>
            
