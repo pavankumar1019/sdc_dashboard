e0cce99512c4ab0e2315a8adf28c89ec6b9d2fd5
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-09-09 11:05:17 +0530

    a

--- bangarpet/staff/model/pages/exportConsolidate.php
+++ bangarpet/staff/model/pages/exportConsolidate.php
@@ -1,54 +1,56 @@
 <div class="pcoded-main-container">
-        <div class="pcoded-wrapper">
-            <div class="pcoded-content">
-                <div class="pcoded-inner-content">
-                    <!-- [ breadcrumb ] start -->
-                    <div class="page-header">
-                        <div class="page-block">
-                            <div class="row align-items-center">
-                                <div class="col-md-12">
-                                    <div class="page-header-title">
-                                        <h5 class="m-b-10">Downoad all consolidates</h5>
+    <div class="pcoded-wrapper">
+        <div class="pcoded-content">
+            <div class="pcoded-inner-content">
+                <!-- [ breadcrumb ] start -->
+                <div class="page-header">
+                    <div class="page-block">
+                        <div class="row align-items-center">
+                            <div class="col-md-12">
+                                <div class="page-header-title">
+                                    <h5 class="m-b-10">Downoad all consolidates</h5>
+                                </div>
+                                <ul class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="./dashboard.php?page=home"><i
+                                                class="feather icon-home"></i></a></li>
+                                    <li class="breadcrumb-item"><a href="javascript:">Export consolidate</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- [ breadcrumb ] end -->
+                <div class="main-body">
+                    <div class="page-wrapper">
+                        <!-- [ Main Content ] start -->
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h5>Select the Test / Exam</h5>
                                     </div>
-                                    <ul class="breadcrumb">
-                                        <li class="breadcrumb-item"><a href="./dashboard.php?page=home"><i class="feather icon-home"></i></a></li>
-                                        <li class="breadcrumb-item"><a href="javascript:">Export consolidate</a></li>
-                                    </ul>
+                                    <div class="card-block">
+                                        <div class="form-group p-2">
+
+                                            <select id="testname_p" class="form-control"
+                                                style="border:1px solid black;">
+                                                <option value="">-Select Test--</option>
+
+                                        </div>
+                                        </p>
+                                    </div>
+                                    <div class="form-group p-2">
+                                        <a class="btn btn-primary" id="print">Download</a>
+                                    </div>
+                                    </p>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <!-- [ breadcrumb ] end -->
-                    <div class="main-body">
-                        <div class="page-wrapper">
-                            <!-- [ Main Content ] start -->
-                            <div class="row">
-                                <div class="col-sm-12">
-                                    <div class="card">
-                                        <div class="card-header">
-                                            <h5>Select the Test / Exam</h5>
-                                        </div>
-                                        <div class="card-block">
-                                        <div class="form-group p-2">
-
-<select id="testname_p" class="form-control" style="border:1px solid black;">
-<option value="">-Select Test--</option>
-
-</div>
-                                            </p>
-                                        </div>
-                                        <div class="form-group p-2">
-<a class="btn btn-primary" id="print">Download</a>
-                                        </div>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- [ Main Content ] end -->
-                        </div>
-                    </div>
+                    <!-- [ Main Content ] end -->
                 </div>
             </div>
         </div>
     </div>
+</div>
+</div>
