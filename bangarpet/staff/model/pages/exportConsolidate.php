42d5f30cde3266e6e265d95bfcd9eaf5159d9e80
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-09-09 11:08:12 +0530

    a

--- bangarpet/staff/model/pages/exportConsolidate.php
+++ bangarpet/staff/model/pages/exportConsolidate.php
@@ -8,7 +8,7 @@
                         <div class="row align-items-center">
                             <div class="col-md-12">
                                 <div class="page-header-title">
-                                    <h5 class="m-b-10">Downoad all consolidates</h5>
+                                    <h5 class="m-b-10">Downoad all consolidate</h5>
                                 </div>
                                 <ul class="breadcrumb">
                                     <li class="breadcrumb-item"><a href="./dashboard.php?page=home"><i
