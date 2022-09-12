163edc9bc60149ff5d61f99f53e65dbb1b8b68ed
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-09-12 13:06:29 +0530

    a

--- bangarpet/staff/export/export.php
+++ bangarpet/staff/export/export.php
@@ -68,11 +68,11 @@ foreach($gettestresult as $gettestresultrow){
   $class=$gettestresultrow['class'];
   $name=$gettestresultrow['name'];
   $minimarks=$gettestresultrow['minmarks'];
-  echo $maximarks;
+
 }
      $query = "SELECT ".$tbl_admission.".StudentName,  ".$tbl_admission.".father_name, ".$tbl_admission.".combination, ".$tbl_admission.".lang_code, ".$tbl_admission.".mobile_no, ".$tbl_admission.".RollNo, ".$class_test_marks.".l1, ".$class_test_marks.".l2,  ".$class_test_marks.".s1, ".$class_test_marks.".s2,  ".$class_test_marks.".s3,  ".$class_test_marks.".s4,  ".$class_test_marks.".total
      FROM ".$tbl_admission."
-     LEFT JOIN ".$class_test_marks." ON ".$tbl_admission.".RollNo = ".$class_test_marks.".roll AND ".$class_test_marks.".test_id=". $_SESSION['test_name']."    WHERE ".$tbl_admission.".Class=".$_SESSION['class_id']."  
+     LEFT JOIN ".$class_test_marks." ON ".$tbl_admission.".RollNo = ".$class_test_marks.".roll AND ".$class_test_marks.".test_id=".$testid."    WHERE ".$tbl_admission.".Class=".$_SESSION['class_id']."  
      ";
  $result = mysqli_query($conn, $query);
 
