5f274f4eebc1fd04f7fe6aefcac05671ea2815b9
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-09-23 13:19:19 +0530

    a

--- bangarpet/staff/export/scriptsendsms.php
+++ bangarpet/staff/export/scriptsendsms.php
@@ -2,7 +2,20 @@
 
 include('../db_bpet_sdc/db.php');
 
-$totalmaxmarks= $_SESSION['maxmarks']*6;
+$testid= $_POST['test_id'];
+$gettest="SELECT * FROM ".$current_test." WHERE id=".$testid."";
+$gettestresult=$conn->query($gettest);
+foreach($gettestresult as $gettestresultrow){
+  $maximarks=$gettestresultrow['maxmarks'];
+  $class=$gettestresultrow['class'];
+  $name=$gettestresultrow['name'];
+  $minimarks=$gettestresultrow['minmarks'];
+
+}
+
+
+
+$totalmaxmarks= $maximarks*6;
 // echo $_GET['class_id']
 $query = "SELECT ".$tbl_admission.".StudentName,  ".$tbl_admission.".father_name, ".$tbl_admission.".combination, ".$tbl_admission.".lang_code, ".$tbl_admission.".mobile_no, ".$tbl_admission.".RollNo, ".$class_test_marks.".l1, ".$class_test_marks.".l2,  ".$class_test_marks.".s1, ".$class_test_marks.".s2,  ".$class_test_marks.".s3,  ".$class_test_marks.".s4,  ".$class_test_marks.".total
 FROM ".$tbl_admission."
@@ -60,11 +73,18 @@ $s3="MT";
 $s4="CS";
 }
 
+// <tr>
+// <td> Name : '.$row['StudentName'].' <br>
+// Message : Dear '.$row['StudentName'].' as scored marks in Term Test - 1 <br>
+// '.$l1.'='.$row['l1'].',EN='.$row['l2'].','.$s1.'='.$row['s1'].','.$s2.'='.$row['s2'].','.$s3.'='.$row['s3'].','.$s4.'='.$row['s3'].' TOTAL='.$row['total'].'.
+// </td>
+// </tr>
+
 $method = 'sendMessage';
 	//  variable
-    $name ="PAVAN KUMAR";
-    $score="KA-100/100EN-100/100PH-100/100MA-100/100CH-100/100BI-100/100";
-    $testname="Unit Test - 1";
+    $name =$row['StudentName'];
+    $score=''.$l1.'-'.$row['l1'].'/'. $maximarks.'EN-'.$row['l2'].'/'. $maximarks.''.$s1.'-'.$row['s1'].'/'. $maximarks.''.$s2.'-'.$row['s2'].'/'. $maximarks.''.$s3.'-'.$row['s3'].'/'. $maximarks.''.$s4.'-'.$row['s4'].'/'. $maximarks.'';
+    $testname=$name;
     // Message details
 $content =  rawurlencode('Dear '.$name.' your score in '.$testname.' is 
 '.$score.'Grand Total :500/600 RESULT : FAIL
