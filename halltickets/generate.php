e32c7dbead751e5cc6eaf9fcedde391424a9a7cc
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-10-10 10:48:02 +0530

    a

--- halltickets/generate.php
+++ halltickets/generate.php
@@ -3,7 +3,54 @@
 //import.php
 
 include './vendor/autoload.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+
+    <style>
+        table,
+        tr,
+        th,
+        td {
+            border: 1px solid black !important;
+
+        }
+
+        th {
+            font-size: 20px;
+        }
+
+        td {
+            font-weight: 600;
+            font-size: 18px;
+        }
+        div.ten:before {
+  content: "";
+  position: absolute;
+  border: 10px dashed #FF0000;
+  top: -8px;
+  bottom: -8px;
+  left: -8px;
+  right: -8px;
+}
+    </style>
+</head>
+
+<body class="p-0">
+
+
+<?php
 $result = array();
 if(isset($_POST['type'])=="import_validate"){
     if($_FILES["file"]["name"] != '')
@@ -25,75 +72,110 @@ if(isset($_POST['type'])=="import_validate"){
 
   $data = $spreadsheet->getActiveSheet()->toArray();
 
-$error=0;
-$msg=array();
-$usercolumn=array();
-$html='';
-if($data[0][0]=="" || $data[0][0]!="name"){
-  $msg[]="name";
-  $error+=1;
-}
-if($data[0][1]=="" || $data[0][1]!="reg_no"){
-  $msg[]="reg_no";
-  $error+=1;
-}
-if($data[0][2]=="" || $data[0][2]!="combination"){
-  $msg[]="combination";
-  $error+=1;
-}
-if($data[0][3]=="" || $data[0][3]!="student_no"){
-  $msg[]="student_no";
-  $error+=1;
-}
-
-for($i=0; $i<sizeof($data[0]); $i++){
-      $usercolumn[]=$data[0][$i];
-}
-if($error !=0){
-  echo "Columns Required ( <b style='color:red;font-weight:bold;'>".implode(", ",$msg)."</b> ). <br> Your file contains - ( 
-    ".implode(", ",$usercolumn)."
-  )";
-}else{
-  $html.= '<div class="table-responsive"  style=" height: 300px;overflow-y:scroll;">
-  <input type="hidden" data-id="deploy" id="deploy_id">
-  <table class="table table-striped">
-  <thead>
-  <tr>
-    <th>name</th>
-    <th>reg_no</th>
-    <th>combination</th>
-    <th>student_no</th>
-    
-  </tr>
-  </thead>
-  <tbody>
-   ';
- 
-   unset($data[0]);
+  unset($data[0]);
+  $i=0;
   foreach($data as $row){
-    $html.='
-    <tr>
-    <td>'.$row[0].'</td>
-    <td>'.$row[1].'</td>
-    <td>'.$row[2].'</td>
-    <td>'.$row[3].'</td>
-   
-  </tr>
-    ';
+$i++;
+?>
+
+    <div style="display: flex; flex-direction:row;" class="text-center mt-2">
+
+        <div class="p-2">
+            <img src="logo.jpg" width="100px" alt="" srcset="">
+        </div>
+        <div class="text-center">
+            <h3>SMT. DANAMMA CHANNABASAVIAH (SDC) <br>
+                INDEPENDENT PU COLLEGE
+            </h3>
+            <h5>Canara Bank Road, Bangarpet-563114</h5>
+            <h4>Exam Admit Card</h4>
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="col-6 text-left">
+         <b>   I / II PU MID-TERM EXAM - SEP 2022</b>
+        </div>
+        <div class="col-6 text-right">
+            <b>College Code : MM0174</b>
+        </div>
+    </div>
+
+    <div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col" colspan="5" class="p-0">Name of the student : <?php echo $row[0]; ?></th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="col" class="p-1">Reg No</th>
+                    <th scope="col" class="p-1">Student No.</th>
+                    <th scope="col" class="p-1" colspan="2">Combination</th>
+
+                </tr>
+                <tr>
+                    <th scope="row" class="p-1">Date</th>
+                    <th scope="row" class="p-1" style="text-align: center;">Day</th>
+                    <th scope="row" class="p-1">Subject</th>
+                    <th scope="row" class="p-1">Invigilator Sign</th>
+                </tr>
+                <tr>
+                    <td scope="row" class="p-1">08-09-2022</td>
+                    <td scope="row" class="p-1" style="text-align: center;">THUR</td>
+                    <td scope="row" class="p-1">Kannada/Hindi/Urdu/Sanskrit</td>
+                    <td scope="row" class="p-1"></td>
+                </tr>
+                <tr>
+                    <td scope="row" class="p-1">09-09-2022</td>
+                    <td scope="row" class="p-1" style="text-align: center;">FIR</td>
+                    <td scope="row" class="p-1">Economics / Basic Maths</td>
+                    <td scope="row" class="p-1"></td>
+                </tr>
+                <tr>
+                    <td scope="row" class="p-1">10-09-2022</td>
+                    <td scope="row" class="p-1" style="text-align: center;">SAT</td>
+                    <td scope="row" class="p-1">Business Studies</td>
+                    <td scope="row" class="p-1"></td>
+                </tr>
+                <tr>
+                    <td scope="row" class="p-1">12-09-2022</td>
+                    <td scope="row" class="p-1" style="text-align: center;">MON</td>
+                    <td scope="row" class="p-1">Accountancy</td>
+                    <td scope="row" class="p-1"></td>
+                </tr>
+                <tr>
+                    <td scope="row" class="p-1">13-09-2022</td>
+                    <td scope="row" class="p-1" style="text-align: center;">TUE</td>
+                    <td scope="row" class="p-1">Statistics / Computer Science</td>
+                    <td scope="row" class="p-1"></td>
+                </tr>
+                <tr>
+                    <td scope="row" class="p-1">14-09-2022</td>
+                    <td scope="row" class="p-1" style="text-align: center;">WED</td>
+                    <td scope="row" class="p-1">English</td>
+                    <td scope="row" class="p-1"></td>
+                </tr>
+            </tbody>
+        </table>
+
+
+    </div>
+    <div class="text-center mb-5">
+        <b>Note:</b> Exam Timings for all subjects <b>8:45AM</b> to <b>11:45AM</b>
+    </div>
+    <div>
+        <h6>Student's Signture</h6>
+        <p class="text-right">
+            <img src="./pricipal sign.png" width="200px" alt=""><br> PRINCIPAL</p>
+    </div>
+
+
+
+<?php
   }
-  $html.= '
-  </tbody>
-  </table>
-  <div>
-  <button class="btn btn-primary" id="button2">Generate Hall Tickets</button>
-
-  </div>
-</div>';
-
-echo $html;
-
-}
-
 
 
  }
@@ -105,3 +187,12 @@ echo $html;
 
 
 ?>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
