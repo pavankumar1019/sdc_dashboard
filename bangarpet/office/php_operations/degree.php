ebc39f0f0f555a0886f7323eacbc94d69d6a1f39
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-10-18 12:05:16 +0530

    a

--- bangarpet/office/php_operations/degree.php
+++ bangarpet/office/php_operations/degree.php
@@ -12,7 +12,7 @@ if($_POST['type']=="loadtotal"){
             <td   style="border:1px solid black;">'.$row['fathername'].'</td>
             <td   style="border:1px solid black;">'.$row['phone_number'].'</td>
             <td   style="border:1px solid black;"><div class="custom-control form-control-lg custom-checkbox">
-          <input type="checkbox" class="check custom-control-input" name="reg_no[]" id="reg_no" value="'.$row['id'].'">
+          <input type="checkbox" class="check custom-control-input"  id="reg_no" value="'.$row['id'].'">
           <label class="custom-control-label" for="reg_no"  style="user-select: none;">Check Here</label>
       </div></td>
           </tr>
