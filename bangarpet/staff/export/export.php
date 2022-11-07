c0e2c79c5a884326c9964a20fe0bb2832c9ee5c7
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-11-07 09:58:57 +0530

    a

--- bangarpet/staff/export/export.php
+++ bangarpet/staff/export/export.php
@@ -159,14 +159,7 @@ $langname="HIN";
       $status="<b style='color:blue'>FAIL</b>";
     }  
   }
- else{
-  if($row["l1"]>=$minimarks && $row["l2"]>=$minimarks && $row["s1"]>=$minimarks && $row["s2"]>=$minimarks && $row["s3"]>=$minimarks && $row["s4"]>=$minimarks){
-    $status="PASS";
-  }else{
-    $status="<b style='color:red'>FAIL</b>";
-  }
 
- }
 
  }
 else{
