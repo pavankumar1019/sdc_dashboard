10af035288b608b3f0c0572ddcceefaab646417a
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-12-18 13:19:05 +0530

    a
    a

--- admin/login_ajax.php
+++ admin/login_ajax.php
@@ -5,48 +5,36 @@
 	if($_POST["type"]==2){
 		    $userId=$_POST['userId'];
             $key=$_POST['key'];
-			$result = mysqli_query($connect,"SELECT * FROM user_office WHERE user_name='$userId' AND user_key='$key'");
-			$row  = mysqli_fetch_array($result);
-            foreach($row as $dt){
-                $phone=$dt['phone_no'];
-            }
-			if(is_array($row)){
-		// Account details
-	$apiKey = urlencode('NDM1OTMzNTA0MjUyMzk2MzVhNGUzMDQ4NzY3NTM5Njc=');
-	$rndno=rand(100000, 999999);
-	// Message details
-	$numbers = array(918123456789);
-	$sender = urlencode('SDCPUC');
-	$message = rawurlencode('Dear # your score in # is 
-#Grand Total 
-SDC College OTP:'.$rndno);
- 
-	$numbers = implode(',', $numbers);
- 
-	// Prepare data for POST request
-	$data = array('apikey' => $apiKey, 'numbers' => $numbers, "sender" => $sender, "message" => $message);
- 
-	// Send the POST request with cURL
-	$ch = curl_init('https://api.textlocal.in/send/');
-	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$response = curl_exec($ch);
-				/* get response */
-				$output = curl_exec($ch);
-				/* Print error if any */
-				if(curl_errno($ch))
-				{
-					echo 'error:' . curl_error($ch);
-				}
-				curl_close($ch);
-				$_SESSION['otp']=$rndno;
-				echo json_encode(array("statusCode"=>200));
-			}
-			else{ 
-				echo "Mobile number not exist !";
-			}
-	}
+			
+            $sql = "SELECT * FROM user_office WHERE user_name='$userId' AND user_key='$key'";
+            $result = $conn->query($sql);
+        
+        if ($result->num_rows > 0) {
+          // output data of each row
+          while($row = $result->fetch_assoc()) {
+            // Fstmsms($sender_id,$message,$number);
+            $number=$row['phone_no'];
+            $otp=rand(100000,999999);
+            Fstmsms($number,$otp);
+            $_SESSION['OTP_staff']=$otp;
+            echo json_encode(array(
+                "statusCode"=>200,
+                "phone"=>substr($row['phone_no'], -3),
+                "name"=>$row['name'],
+                "branch"=>$row['branch'],
+                "role"=>$row['role'],
+                "img"=>$row['photo'],
+                "id"=>$row['id'],
+                "class"=>$row['class'],
+                "class_id"=>$row['class_id'],
+                "value"=>$otp
+            ));
+          }
+        } 
+        else {
+            echo json_encode(array("statusCode"=>201));
+        }
+        }
 	if($_POST["type"]==3){
 		if($_POST["otp"]==$_SESSION['otp']){
 			echo json_encode(array("statusCode"=>200));
