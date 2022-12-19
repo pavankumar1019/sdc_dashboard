423ff5b0b8503a9be3c0d0491168b4931b46e91d
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-12-19 10:08:41 +0530

    a
    a

--- admin/login_ajax.php
+++ admin/login_ajax.php
@@ -2,9 +2,9 @@
 
 session_start();
 include ('./db_bpet_sdc/db.php');
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
     
 	if($_POST["type"]==2){
 		    $userId=$_POST['userId'];
@@ -44,6 +44,33 @@ include ('./db_bpet_sdc/db.php');
 
 
 
-
+  // function to login 
+  function Fstmsms($number,$otp){
+ 	// Account details
+   $apiKey = urlencode('NDM1OTMzNTA0MjUyMzk2MzVhNGUzMDQ4NzY3NTM5Njc=');
+	
+   // Message details
+   $numbers = array($number);
+   $sender = urlencode('SDCPUC');
+   $message = rawurlencode('Your OTP : '.$otp.' Do not share OTP
+   SDC College Bangarpet');
+   
+   $numbers = implode(',', $numbers);
+  
+   // Prepare data for POST request
+   $data = array('apikey' => $apiKey, 'numbers' => $numbers, "sender" => $sender, "message" => $message);
+  
+   // Send the POST request with cURL
+   $ch = curl_init('https://api.textlocal.in/send/');
+   curl_setopt($ch, CURLOPT_POST, true);
+   curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+   $response = curl_exec($ch);
+   curl_close($ch);
+   
+   // Process your response here
+  //  echo $response;
+  
+  }
   
 ?>
