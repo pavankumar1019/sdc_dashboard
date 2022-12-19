637c88a141b5e20b3d7c44d6ab15ab35f662ced6
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-12-19 11:51:38 +0530

    a
    a

--- admin/model/pages/sms.php
+++ admin/model/pages/sms.php
@@ -132,11 +132,11 @@
                                                 <span class="custom-control-indicator"></span>
                                             </label>
                                         </th>
-                                        <th scope="col">Detailes</th>
-                                        <th scope="col">Fixation</th>
-                                        <th scope="col">Paid Detailes</th>
+                                        <th scope="col">Date</th>
+                                        <th scope="col">To</th>
+                                        <th scope="col">From</th>
+                                        <th scope="col">Message</th>
                                         <th scope="col">Status</th>
-                                        <th scope="col">Actions</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -167,87 +167,9 @@
                                             <button class="btn btn-sm btn-danger">Delete</button>
                                         </td>
                                     </tr>
-                                    <tr>
-                                        <th scope="row">
-                                            <label class="custom-control custom-checkbox m-0 p-0">
-                                                <input type="checkbox" class="custom-control-input table-select-row">
-                                                <span class="custom-control-indicator"></span>
-                                            </label>
-                                        </th>
-                                        <td>
-                                            <h2>PAVAN KUMAR S</h2>
-                                            SRINIVASA <br>
-                                            1 PUC EBACS
-                                        </td>
-                                        <td>
-                                            <h5><b>₹30,000 </b></h1>
-                                        </td>
-                                        <td>
-                                            20-01-2022:9:22:AM - <b>₹10,000</b> <br>
-                                            20-01-2022:9:22:AM - <b>₹10,000</b> <br>
-                                        </td>
-                                        <td>
-                                            <span class="badge badge-pill badge-primary">Balance</span>
-                                        </td>
-                                        <td>
-                                            <button class="btn btn-sm btn-primary">Collect</button>
-                                            <button class="btn btn-sm btn-danger">Delete</button>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">
-                                            <label class="custom-control custom-checkbox m-0 p-0">
-                                                <input type="checkbox" class="custom-control-input table-select-row">
-                                                <span class="custom-control-indicator"></span>
-                                            </label>
-                                        </th>
-                                        <td>
-                                            <h2>PAVAN KUMAR S</h2>
-                                            SRINIVASA <br>
-                                            1 PUC EBACS
-                                        </td>
-                                        <td>
-                                            <h5><b>₹30,000 </b></h1>
-                                        </td>
-                                        <td>
-                                            20-01-2022:9:22:AM - <b>₹10,000</b> <br>
-                                            20-01-2022:9:22:AM - <b>₹10,000</b> <br>
-                                        </td>
-                                        <td>
-                                            <span class="badge badge-pill badge-primary">Balance</span>
-                                        </td>
-                                        <td>
-                                            <button class="btn btn-sm btn-primary">Collect</button>
-                                            <button class="btn btn-sm btn-danger">Delete</button>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">
-                                            <label class="custom-control custom-checkbox m-0 p-0">
-                                                <input type="checkbox" class="custom-control-input table-select-row">
-                                                <span class="custom-control-indicator"></span>
-                                            </label>
-                                        </th>
-                                        <td>
-                                            <h2>PAVAN KUMAR S</h2>
-                                            SRINIVASA <br>
-                                            1 PUC EBACS
-                                        </td>
-                                        <td>
-                                            <h5><b>₹30,000 </b></h1>
-                                        </td>
-                                        <td>
-                                            20-01-2022:9:22:AM - <b>₹10,000</b> <br>
-                                            20-01-2022:9:22:AM - <b>₹10,000</b> <br>
-                                        </td>
-                                        <td>
-                                            <span class="badge badge-pill badge-primary">Balance</span>
-                                        </td>
-                                        <td>
-                                            <button class="btn btn-sm btn-primary">Collect</button>
-                                            <button class="btn btn-sm btn-danger">Delete</button>
-                                        </td>
-                                    </tr>
+                                   
+                                    
+                                   
 
                                 </tbody>
                             </table>
