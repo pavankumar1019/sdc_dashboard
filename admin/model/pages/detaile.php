25f43a6c9256659970cb08a9f73982fa3a9d9549
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-12-19 13:03:15 +0530

    A
    A

--- admin/model/pages/detaile.php
+++ admin/model/pages/detaile.php
@@ -65,7 +65,7 @@
                             </svg>
                         </div>
                         <div class="card-body">
-                            <div class="card-info-title">SMS Credits</div>
+                            <div class="card-info-title">COLLECT FEE</div>
                             <h3 class="card-title mb-0"></h3>
                         </div>
                     </div>
@@ -84,7 +84,7 @@
                             </svg>
                         </div>
                         <div class="card-body">
-                            <div class="card-info-title">SMS Credits</div>
+                            <div class="card-info-title">ADMIT STUDENT</div>
                             <h3 class="card-title mb-0"></h3>
                         </div>
                     </div>
@@ -103,7 +103,26 @@
                             </svg>
                         </div>
                         <div class="card-body">
-                            <div class="card-info-title">SMS Credits</div>
+                            <div class="card-info-title">ADMIT STAFF</div>
+                            <h3 class="card-title mb-0"></h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3 d-flex bounceIn" onClick="location.href='./dashboard.php?page=sms'">
+                <div class="card border-0 bg-primary text-white text-center mb-grid w-100">
+                    <div class="d-flex flex-row align-items-center h-100">
+                        <div class="card-icon d-flex align-items-center h-100 justify-content-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" class="feather feather-layers">
+                                <polygon points="12 2 2 7 12 12 22 7 12 2"></polygon>
+                                <polyline points="2 17 12 22 22 17"></polyline>
+                                <polyline points="2 12 12 17 22 12"></polyline>
+                            </svg>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-info-title">CREATE ACADEMICS</div>
                             <h3 class="card-title mb-0"></h3>
                         </div>
                     </div>
@@ -122,26 +141,7 @@
                             </svg>
                         </div>
                         <div class="card-body">
-                            <div class="card-info-title">SMS Credits</div>
-                            <h3 class="card-title mb-0"></h3>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-3 d-flex bounceIn" onClick="location.href='./dashboard.php?page=sms'">
-                <div class="card border-0 bg-secondary text-white text-center mb-grid w-100">
-                    <div class="d-flex flex-row align-items-center h-100">
-                        <div class="card-icon d-flex align-items-center h-100 justify-content-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round" class="feather feather-layers">
-                                <polygon points="12 2 2 7 12 12 22 7 12 2"></polygon>
-                                <polyline points="2 17 12 22 22 17"></polyline>
-                                <polyline points="2 12 12 17 22 12"></polyline>
-                            </svg>
-                        </div>
-                        <div class="card-body">
-                            <div class="card-info-title">SMS Credits</div>
+                            <div class="card-info-title">SMS </div>
                             <h3 class="card-title mb-0"></h3>
                         </div>
                     </div>
