810cc12dc6e7030654a0383cd0f5fc5f0d18a5e9
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-12-19 12:00:21 +0530

    a
    a

--- test.php
+++ test.php
@@ -1,24 +1,16 @@
 <?php
-
-$method = 'sendMessage';
-	//  variable
-    $name ="PAVAN KUMAR";
-    $score="KA-100/100EN-100/100PH-100/100MA-100/100CH-100/100BI-100/100";
-    $testname="Unit Test - 1";
-    // Message details
-$content =  rawurlencode('Dear '.$name.' your score in '.$testname.' is 
-'.$score.'Grand Total :500/600 RESULT : FAIL
-SDC College BANGARPET-563114');
-
-    
-    
-    // Prepare data for POST request
-    
-    // Send the POST request with cURL
-    $ch = curl_init('https://smsforall.com/portal/receive_api/api_request?method=sendMessage&mobileno=7483737698&content='.$content.'&loginid=Sdcbpet2&auth_scheme=PLAIN&password=Sajsdc@25&senderid=SDCPUC');
-    curl_setopt($ch, CURLOPT_POST, false);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $response = curl_exec($ch);
-    curl_close($ch);
-    echo $name."&nbsp;-&nbsp; Message Sent";
+	// Account details
+	$apiKey = urlencode('NDM1OTMzNTA0MjUyMzk2MzVhNGUzMDQ4NzY3NTM5Njc=');
+	// Prepare data for POST request
+	$data = array('apikey' => $apiKey);
+	// Send the POST request with cURL
+	$ch = curl_init('https://api.textlocal.in/get_history_api/');
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$report = curl_exec($ch);
+	curl_close($ch);
+	
+	// Process your response here
+	var_dump( $report['total']);
 ?>
