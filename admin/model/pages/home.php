9cc5a8aad919853750f03484505c38c16d929bb0
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-12-19 11:48:31 +0530

    a
    a

--- admin/model/pages/home.php
+++ admin/model/pages/home.php
@@ -75,7 +75,7 @@
                 </div>
               </div>
 
-              <div class="col-md-6 col-lg-3 d-flex bounceIn" href="./dashboard.php?page=sms">
+              <div class="col-md-6 col-lg-3 d-flex bounceIn" id="sms_div" onClick="location.href='./dashboard.php?page=home'">
                 <div class="card border-0 bg-secondary text-white text-center mb-grid w-100">
                   <div class="d-flex flex-row align-items-center h-100">
                     <div class="card-icon d-flex align-items-center h-100 justify-content-center">
