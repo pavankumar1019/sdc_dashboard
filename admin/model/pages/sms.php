13e91c6f36bdc54875d29f6b016e25d659d95e86
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-12-19 12:42:15 +0530

    a
    a

--- admin/model/pages/sms.php
+++ admin/model/pages/sms.php
@@ -156,28 +156,4 @@
         </div>
     </div>
 </div>
-<!-- Modal -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalCenterTitle">Collect Fee</h5>
-        <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button> -->
-      </div>
-      <div class="modal-body">
-       <h2>PAVAN KUMAR S</h2>
-       <h4>Fixation : <b><?php
-       setlocale(LC_MONETARY,"en_IN");
-       echo money_format(" %i", "30000");
-       ?></b></h4>
-       <input type="text" class="form-control" onkeypress="return isNumber(event)"  id="enterammount" name="Ammount" style="font-size:100px" id="">
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Collect</button>
-      </div>
-    </div>
-  </div>
-</div>
+
