3c28fcaf085182e2a19452e6fbafffdf0e790a56
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-12-28 14:06:48 +0530

    a
    a

--- test.php
+++ test.php
@@ -2,7 +2,19 @@
 	// Account details
 	$apiKey = urlencode('NDM1OTMzNTA0MjUyMzk2MzVhNGUzMDQ4NzY3NTM5Njc=');
 	// Prepare data for POST request
-	$data = array('apikey' => $apiKey);
+	$sender = urlencode('SDCDGR');
+
+
+	$numbers = "7483737698";
+
+$content =  rawurlencode('Dear '.$name.' your score in '.$testname.' is 
+'.$score.'Grand Total :'.$row['total'].'/'.$totalmaxmarks.' RESULT : '.$status.'
+SDC College BANGARPET-563114');
+
+
+
+// Prepare data for POST request
+$data = array('apikey' => $apiKey, 'numbers' => $numbers, "sender" => $sender, "message" => $content);
 	// Send the POST request with cURL
 	$ch = curl_init('https://api.textlocal.in/get_history_api/');
 	curl_setopt($ch, CURLOPT_POST, true);
