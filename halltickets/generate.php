0bb4cbf616a6d9ce053b9bf641d7aa2567eb7073
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-12-29 14:21:16 +0530

    A
    A

--- halltickets/generate.php
+++ halltickets/generate.php
@@ -261,7 +261,7 @@ if($i % 2 == 0){
     <section class="line p-5">
         <div style="margin-top:200px;">
             <h5 align="center">INSTRUCTIONS TO THE STUDENTS</h5>
-            <p style="font-size:20px;">
+            <p style="font-size:22px;">
 
                 1) Make yourself present at the exam hall 30 minutes before the commencement of examination. <br>
                 2) You should bring the Exam Hall Ticket and Identity card on all the days of examination. You will not
