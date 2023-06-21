83034088c8c3405d04baa7a4cc1d06a7e872dce7
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2023-06-21 15:07:41 +0530

    Update dashboard.php

--- admin/model/dashboard.php
+++ admin/model/dashboard.php
@@ -1,15 +1,4 @@
 
-      <?php $page = isset($_GET['page']) ? $_GET['page']:'home' ?>
-            <?php
-            $title = isset($_GET['page']) ? ucwords(str_replace("_", ' ', $_GET['page'])) : "home";
-             ?>
-            <?php 
-            if(!file_exists('./model/pages/'.$page.".php")){
-              include './404.html';
-            }else{
-            include './model/pages/'.$page.'.php';
-            }
-          ?>
             <!-- menu active function -->
             <?php
 // function active($currect_page){
